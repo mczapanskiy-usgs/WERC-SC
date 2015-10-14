6d84f7422c56b60d3a7df9df250f6ef1416deb04
Max Czapanskiy <mczapanskiy@usgs.gov>
2015-10-14 10:48:33 -0700

    With Speed plot

--- dive_identification/src/dive-identification.R
+++ dive_identification/src/dive-identification.R
@@ -1,6 +1,5 @@
 library('plyr')
 library('dplyr')
-library('ggplot2')
 
 test_deploy_id = 341 # LEH2014RFBO004A
 
@@ -39,17 +38,25 @@ calibrate.tdr <- function(tdr) {
             mutate(fastlog, 
                    Surface = median(PressureInit),
                    Pressure = pmax(PressureInit - Surface, 0),
-                   Dive = Pressure >= 1)
+                   Dive = Pressure >= 1,
+                   Speed = c(diff(Pressure) / (UTC %>% diff %>% round(1) %>% as.numeric), NA))
           })
 }
 
 test_deploy_id %>% initialize.tdr() %>% calibrate.tdr() -> test_tdr
 test_tdr %>%
   group_by(EventId) %>%
-  summarize(records = n()) %>%
+  summarize(records = n(),
+            max_depth = max(Pressure)) %>%
   arrange(desc(records)) %>%
   View
-with(filter(test_tdr, EventId == 466), {
+
+verify.tdr <- function()
+
+x11_scale = 1.5
+x11(width = 11 * x11_scale, height = 7 * x11_scale)
+par(mfrow = c(2,1))
+with(filter(test_tdr, EventId == 322), {
   plot(UTC,
        Pressure,
        col = ifelse(Dive, 'blue', 'red'),
@@ -64,3 +71,17 @@ with(filter(test_tdr, EventId == 466), {
   abline(h = 0, col = '#AAAAAA', lty = 2)
   abline(h = 1, col = '#AAAAAA', lty = 2)
 })
+with(filter(test_tdr, EventId == 322), {
+  plot(UTC,
+       Speed,
+       pch = 16,
+       xaxt = "n",
+       xlab = "") 
+  lines(UTC, Speed)
+  axis.POSIXct(1, UTC, format = "%m/%d %H:%M:%OS1")
+  axis(4)
+  grid()
+  abline(h = 0, col = '#AAAAAA', lty = 2)
+  abline(h = 1, col = '#AAAAAA', lty = 2)
+})
+dev.off()
