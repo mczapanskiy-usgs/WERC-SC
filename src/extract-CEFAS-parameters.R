8ea2ea9fce71f330e54379411fe9454421409bd9
Max Czapanskiy <mczapanskiy@usgs.gov>
2015-10-15 16:20:35 -0700

    Modify extract-CEFAS-parameters.R
    Extract threshold and wet/dry

--- src/extract-CEFAS-parameters.R
+++ src/extract-CEFAS-parameters.R
@@ -1,6 +1,6 @@
 library('dplyr')
 
-get.datablock0.rate <- function(filepath = 'dive_identification/rttr_data/LEH2014RTTR036B_A10372_071814.CSV') {
+get.datablock0.rate <- function(filepath = 'dive_identification/CEFAS_output/HAW_WTSH_007_02182_A10346_101414.CSV') {
   if(file.exists(filepath)) {
     readLines(filepath) %>%
       grep(pattern = 'Logging rate = \\d+', x = ., value = TRUE) %>%
@@ -11,7 +11,7 @@ get.datablock0.rate <- function(filepath = 'dive_identification/rttr_data/LEH201
   }
 }
 
-get.fastlog.rate <- function(filepath = 'dive_identification/rttr_data/LEH2014RTTR036B_A10372_071814.CSV') {
+get.fastlog.rate <- function(filepath = 'dive_identification/CEFAS_output/HAW_WTSH_007_02182_A10346_101414.CSV') {
   if(file.exists(filepath)) {
     readLines(filepath) %>%
       grep(pattern = 'Fast rate [0-9\\.]+', x = ., value = TRUE) %>%
@@ -23,19 +23,43 @@ get.fastlog.rate <- function(filepath = 'dive_identification/rttr_data/LEH2014RT
   }
 }
 
+get.threshold <- function(filepath = 'dive_identification/CEFAS_output/HAW_WTSH_007_02182_A10346_101414.CSV') {
+  if(file.exists(filepath)) {
+    readLines(filepath) %>%
+      grep(pattern = 'Dive Termination Depth = [0-9\\.]+m', x = ., value = TRUE) %>%
+      sub('Dive Termination Depth = ([0-9\\.]+)m', '\\1', x = .) %>%
+      as.numeric
+  } else {
+    NA
+  }
+}
+
+get.wetdry <- function(filepath = 'dive_identification/CEFAS_output/LEH2014RFBO020_A10367_061614.CSV') {
+  if(file.exists(filepath)) {
+    readLines(filepath) %>%
+      grep(pattern = 'Wet dry logging -  Active', x = ., value = TRUE) %>%
+      length > 0
+  } else {
+    NA
+  }
+}
+
 library('foreach')
 library('iterators')
 
 get.filepath <- function(species, tdrfile) {
-  file.path('dive_identification', 
-           sprintf('%s_data', tolower(species)),
+  file.path('dive_identification',
+            'CEFAS_output',
            sprintf('%s.CSV', tdrfile))
 }
 
 tdr_metadata <- read.csv('dive_identification/TDRmetadata.csv', stringsAsFactors = FALSE)[,1:7]
 foreach(deployment = iter(tdr_metadata, by = 'row'), .combine = rbind) %do% {
-  data.frame(DataBlock0Rate = get.datablock0.rate(get.filepath(deployment$Species, deployment$TDRFilename)),
-             FastLogRate = get.fastlog.rate(get.filepath(deployment$Species, deployment$TDRFilename)))
+  tdrfile <- get.filepath(deployment$Species, deployment$TDRFilename)
+  data.frame(DataBlock0Rate = get.datablock0.rate(tdrfile),
+             FastLogRate = get.fastlog.rate(tdrfile),
+             Threshold = get.threshold(tdrfile),
+             WetDry = get.wetdry(tdrfile))
 } %>% 
   cbind(tdr_metadata, .) %>%
   write.csv('dive_identification/TDRmetadata2.csv', row.names = FALSE)
