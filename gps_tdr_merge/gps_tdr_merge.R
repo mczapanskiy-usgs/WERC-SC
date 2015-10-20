e66cd49ee0ff59ea6a02fa6ea797472bc2dfcd7e
Max Czapanskiy <mczapanskiy@usgs.gov>
2015-10-20 15:38:02 -0700

    Modify gps_tdr_merge.R
    Make use of data.table foverlap to do overlap join between tdr and gps
    data

--- gps_tdr_merge/gps_tdr_merge.R
+++ gps_tdr_merge/gps_tdr_merge.R
@@ -1,32 +1,22 @@
+library('data.table')
 library('dplyr')
 
-gps <- data.frame(gpsUTC = as.POSIXct('2015-07-07 12:12:12', tz = 'UTC') + 120*(0:9),
+gps <- data.table(gpsUTC = as.POSIXct('2015-07-07 12:12:12', tz = 'UTC') + 120*(0:9),
                   lat = 1:10,
-                  lon = 1:10) 
+                  lon = 1:10) %>%
+  mutate(nextUTC = lead(gpsUTC, default = Inf)) %>%
+  setkey(gpsUTC, nextUTC)
 
-tdr <- data.frame(tdrUTC = as.POSIXct('2015-07-07 12:15:12', tz = 'UTC') + 0:179,
-                  depth = sin(1:180) + 1)
+tdr <- data.table(tdrUTC = as.POSIXct('2015-07-07 12:15:12', tz = 'UTC') + 0:179,
+                  depth = sin(1:180) + 1) %>%
+  mutate(dummyUTC = tdrUTC) %>%
+  setkey(tdrUTC, dummyUTC)
 
-merge(gps%>%
-        mutate(nextUTC = lead(gpsUTC)), 
-      tdr, all = TRUE) %>%
-  filter(tdrUTC >= gpsUTC,
-         tdrUTC < nextUTC) %>%
-  select(-nextUTC) %>% View
-
-
-gps_tdr <- rbind(tdr[gps, roll = 1], gps[tdr, roll = TRUE])
-setkey(gps_tdr, gpsUTC, tdrUTC)
-gps_tdr %>%
-  select(gpsUTC, tdrUTC, lat, lon, depth)
-
-gps[tdr, roll = TRUE] %>%
-  select(gpsUTC, tdrUTC, lat, lon, depth) %>%
-  rbind(gps %>%
-          select)
-
-rbindlist(list(gps, tdr[gps, roll = TRUE]), use.names = TRUE, fill = TRUE) %>%
-  setkey(gpsUTC, tdrUTC) %>% View
-
-gps[tdr, roll = TRUE] %>% setkey(UTC) %>% View
-tdr[gps] %>% setkey(UTC) %>% View
+foverlaps(x = gps,
+          y = tdr,
+          nomatch = NA) %>%
+  select(gpsUTC, 
+         lat, 
+         lon,
+         tdrUTC, 
+         depth)
