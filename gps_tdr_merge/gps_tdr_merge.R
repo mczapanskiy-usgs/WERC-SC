f8d6df7fa8830d34bb0951c2b7b0f945cf2c7fd7
Max Czapanskiy <mczapanskiy@usgs.gov>
2015-10-20 16:27:13 -0700

    Modify gps_tdr_merge.R
    Use sample data in prototype

--- gps_tdr_merge/gps_tdr_merge.R
+++ gps_tdr_merge/gps_tdr_merge.R
@@ -1,6 +1,7 @@
 library('data.table')
 library('dplyr')
 
+# Dummy Data Prototype
 gps <- data.table(gpsUTC = as.POSIXct('2015-07-07 12:12:12', tz = 'UTC') + 120*(0:9),
                   lat = 1:10,
                   lon = 1:10) %>%
@@ -20,3 +21,30 @@ foverlaps(x = gps,
          lon,
          tdrUTC, 
          depth)
+
+
+# Sample Data Prototype
+gps <- read.csv('trackcode/gps/RTTR_trips.csv') %>%
+  filter(Deploy_ID == 290) %>%
+  mutate(gpsUTC = as.POSIXct(UTC, tz ='UTC'),
+         nextUTC = lead(gpsUTC, default = Inf)) %>%
+  data.table %>%
+  setkey(gpsUTC, nextUTC)
+
+tdr <- read.csv('dive_identification/3_dive_data/290.CSV') %>%
+  mutate(diveUTC = as.POSIXct(Begin, tz = 'UTC'),
+         dummyUTC = diveUTC) %>% 
+  data.table %>%
+  setkey(diveUTC, dummyUTC)
+
+foverlaps(x = gps,
+          y = tdr,
+          nomatch = NA) %>%
+  select(gpsUTC,
+         Latitude,
+         Longitude,
+         trip_no,
+         diveUTC,
+         DiveID,
+         DiveDuration = Duration,
+         MaxDepth)
