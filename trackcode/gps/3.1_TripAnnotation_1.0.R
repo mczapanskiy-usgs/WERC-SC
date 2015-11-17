d5c62d0f14bcc7f3ccc33df946c5146ca91c858e
Bill Henry <rhenry@usgs.gov>
2015-11-17 11:25:36 -0800

    Revert "update to deployment-summary"
    This reverts commit e05ecfcf2e329b29283ea09b0e9a73c4746c83b6.

--- trackcode/gps/3.1_TripAnnotation_1.0.R
+++ trackcode/gps/3.1_TripAnnotation_1.0.R
@@ -10,9 +10,9 @@ library('plyr')
 library('dplyr')
 
 # select a spp
-spp="BRBO"
+spp="WTSH"
 
-radius="1.5"
+radius="1"
 
 filename=paste(spp,radius,sep='_')
 
@@ -69,6 +69,6 @@ if (at.nest$Nest_loc_use[i]==0) {
 }
 
 
-write.table(annotated.tracks, paste(dir.out,filename,'_trips_annotated.csv',sep = ""),sep=",",quote=FALSE,col.names=TRUE,row.names=FALSE)
+write.table(annotated.tracks, paste(dir.out,filename,'_trips_annoted.csv',sep = ""),sep=",",quote=FALSE,col.names=TRUE,row.names=FALSE)
 
 # END
