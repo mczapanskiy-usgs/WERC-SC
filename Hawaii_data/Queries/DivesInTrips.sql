80289eee62cd04276a39aa318514c58ae5761721
Max Czapanskiy <mczapanskiy@usgs.gov>
2015-12-21 19:19:26 -0800

    Add DiveInTrips.sql
    Query for joining dives with trips by start time of dive falling within
    trip bounds

---
+++ Hawaii_data/Queries/DivesInTrips.sql
@@ -0,0 +1,17 @@
+SELECT D.DeployID, D.DiveID, D.Begin 'DiveBegin', D.Duration, D.MaxDepth 'DiveDepth',
+	T.TripID, T.Begin 'TripBegin', T.End 'TripEnd', 
+	BM.Species
+FROM Dive D
+	JOIN Trip T
+		ON D.DeployID = T.DeployID
+			AND D.Begin >= T.Begin
+			AND D.Begin < T.End
+	JOIN DeploymentMetadata DM
+		ON DM.DeployID = D.DeployID
+	JOIN BirdMetadata BM
+		ON BM.DeployID = D.DeployID
+WHERE DM.TDRRecovered = 1
+	AND DM.GPSRecovered = 1
+	AND T.BeginComplete = 1
+	AND T.EndComplete = 1
+	
