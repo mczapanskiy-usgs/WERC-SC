34f3b969ca1f6b6ae099472fee7b7843d948ee22
Max Czapanskiy <mczapanskiy@usgs.gov>
2015-12-22 11:36:16 -0800

    Lat/Lon in adehabitat workaround
    We have lat lon data, adehabitat assumes xy data (e.g. UTM). One
    possible workaround is to rewrite portions of adehabitat to use
    distHaversine and bearing instead of xy calculations. It's getting out
    of hand though, so I'm going to try an alternative: projecting lat lon
    data into UTM. This is a checkpoint.

--- src/MHI-database-generator.R
+++ src/MHI-database-generator.R
@@ -2,6 +2,8 @@ library(dplyr)
 library(RSQLite)
 library(foreach)
 library(data.table)
+library(adehabitatLT)
+library(geosphere)
 
 ## Create SQLite Database for MHI data from CSV files
 # Read metadata CSV file
@@ -160,21 +162,21 @@ Dive <- rbind(BRBODive, RFBODive)
 # Gather track and trip data
 BRBOTracks <- read.csv('trackcode/gps/All_tracks/BRBO_1.5_trips_annotated.csv') %>%
   transmute(DeployID = Deploy_ID,
-            UTC = as.POSIXct(UTC, tz = 'UTC') %>% as.numeric,
+            UTC = as.POSIXct(UTC, tz = 'UTC'),
             Latitude,
             Longitude,
             TripID = trip_no)
 
 RFBOTracks <- read.csv('trackcode/gps/All_tracks/RFBO_1.5_trips_annotated.csv') %>%
   transmute(DeployID = Deploy_ID,
-            UTC = as.POSIXct(UTC, tz = 'UTC') %>% as.numeric,
+            UTC = as.POSIXct(UTC, tz = 'UTC'),
             Latitude,
             Longitude,
             TripID = trip_no)
 
 Track <- rbind(BRBOTracks, RFBOTracks)
 
-## Remove duplicate track points. Temporary workaround until trip breaker interpolation bug is fixed..
+## Remove duplicate track points. Temporary workaround until trip breaker interpolation bug is fixed.
 DropTrackPoints <- Track %>% 
   group_by(DeployID, UTC) %>% 
   summarize(N = n()) %>% 
@@ -184,7 +186,7 @@ DropTrackPoints <- Track %>%
   arrange(DeployID, UTC, TripID) %>% 
   mutate(Drop = row_number() %% 2 == 0) %>% 
   filter(Drop) %>% 
-  select(-Drop)
+  dplyr::select(-Drop)
 Track <- anti_join(Track,
                    DropTrackPoints)
 Trip <- read.csv('trackcode/gps/All_tracks/AllSpecies_tripInfo_QAQC.csv',
@@ -200,6 +202,142 @@ Trip <- read.csv('trackcode/gps/All_tracks/AllSpecies_tripInfo_QAQC.csv',
   mutate(Begin = as.POSIXct(Begin, tz = 'UTC', format = '%m/%d/%Y %H:%M') %>% as.numeric,
          End = as.POSIXct(End, tz = 'UTC', format = '%m/%d/%Y %H:%M') %>% as.numeric) %>%
   as.data.frame
+
+# Rediscretize track data
+## We have to edit how as.ltraj calculates dx and dy because we're using lat/lon, not UTM.
+my.as.ltraj <- function (xy, date = NULL, id, burst = id, typeII = TRUE, slsp = c("remove", "missing"), 
+                         infolocs = data.frame(pkey = paste(id, date, sep = "."), row.names = row.names(xy))) 
+{
+  if (typeII) {
+    if (!inherits(date, "POSIXct")) 
+      stop("For objects of type II,\n date should be of class \"POSIXct\"")
+  }
+  else {
+    date <- 1:nrow(xy)
+  }
+  if (length(date) != nrow(xy)) 
+    stop("date should be of the same length as xy")
+  slsp <- match.arg(slsp)
+  if (!is.null(infolocs)) {
+    if (nrow(infolocs) != nrow(xy)) 
+      stop("infolocs should have the same number of rows as xy")
+  }
+  if (length(id) == 1) 
+    id <- factor(rep(as.character(id), nrow(xy)))
+  if (length(id) != nrow(xy)) 
+    stop("id should be of the same length as xy, or of length 1")
+  if (min(table(id)) == 0) 
+    stop("some id's are not present in the data")
+  if (length(burst) == 1) 
+    burst <- factor(rep(as.character(burst), nrow(xy)))
+  if (length(burst) != nrow(xy)) 
+    stop("burst should be of the same length as xy, or of length 1")
+  if (min(table(burst)) == 0) 
+    stop("some bursts are not present in the data")
+  id1 <- factor(id)
+  burst1 <- factor(burst)
+  if (!all(apply(table(id1, burst1) > 0, 2, sum) == 1)) 
+    stop("one burst level should belong to only one id level")
+  x <- xy[, 1]
+  y <- xy[, 2]
+  res <- split(data.frame(x = x, y = y, date = date, row.names = row.names(xy)), 
+               burst)
+  liid <- split(id, burst)
+  if (!is.null(infolocs)) 
+    linfol <- split(infolocs, burst)
+  if (!is.null(infolocs)) 
+    linfol <- lapply(1:length(linfol), function(j) linfol[[j]][order(res[[j]]$date), 
+                                                               , drop = FALSE])
+  res <- lapply(res, function(y) y[order(y$date), , drop = FALSE])
+  rr <- any(unlist(lapply(res, function(x) (length(unique(x$date)) != 
+                                              length(x$date)))))
+  if (rr) 
+    stop("non unique dates for a given burst")
+  x <- xy[, 1]
+  y <- xy[, 2]
+  resbb <- split(data.frame(x = x, y = y, date = date, row.names = row.names(xy)), 
+                 id1)
+  rr <- any(unlist(lapply(resbb, function(x) (length(unique(x$date)) != 
+                                                length(x$date)))))
+  if (rr) 
+    stop("non unique dates for a given id")
+  foo <- function(x) {
+    x1 <- x[-1, ]
+    x2 <- x[-nrow(x), ]
+    # Original distance calculations (assumes UTM)
+    # dist <- c(sqrt((x1$x - x2$x)^2 + (x1$y - x2$y)^2), NA)
+    # R2n <- (x$x - x$x[1])^2 + (x$y - x$y[1])^2
+    # dx <- distHaversine() c(x1$x - x2$x, NA)
+    # dy <- c(x1$y - x2$y, NA)
+    # abs.angle <- ifelse(dist < 1e-07, NA, atan2(dy, dx))
+    # New distance calculation (uses lat/lon)
+    dist <- c(distHaversine(x1[,c('x','y')], x2[,c('x','y')]), NA)
+    R2n <- distHaversine(x[1,c('x','y')], x[,c('x','y')])
+    dt <- c(unclass(x1$date) - unclass(x2$date), NA)
+    abs.angle <- c(bearing(x1, x2), NA)
+    dx <- dist * cos(bearing)
+    dy <- dist * sin(bearing)
+    so <- cbind.data.frame(dx = dx, dy = dy, dist = dist, 
+                           dt = dt, R2n = R2n, abs.angle = abs.angle)
+    return(so)
+  }
+  speed <- lapply(res, foo)
+  res <- lapply(1:length(res), function(i) cbind(res[[i]], 
+                                                 speed[[i]]))
+  ang.rel <- function(df, slspi = slsp) {
+    ang1 <- df$abs.angle[-nrow(df)]
+    ang2 <- df$abs.angle[-1]
+    if (slspi == "remove") {
+      dist <- c(sqrt((df[-nrow(df), "x"] - df[-1, "x"])^2 + 
+                       (df[-nrow(df), "y"] - df[-1, "y"])^2), NA)
+      wh.na <- which(dist < 1e-07)
+      if (length(wh.na) > 0) {
+        no.na <- (1:length(ang1))[!(1:length(ang1)) %in% 
+                                    wh.na]
+        for (i in wh.na) {
+          indx <- no.na[no.na < i]
+          ang1[i] <- ifelse(length(indx) == 0, NA, ang1[max(indx)])
+        }
+      }
+    }
+    res <- ang2 - ang1
+    res <- ifelse(res <= (-pi), 2 * pi + res, res)
+    res <- ifelse(res > pi, res - 2 * pi, res)
+    return(c(NA, res))
+  }
+  rel.angle <- lapply(res, ang.rel)
+  res <- lapply(1:length(res), function(i) data.frame(res[[i]], 
+                                                      rel.angle = rel.angle[[i]]))
+  res <- lapply(1:length(res), function(i) {
+    x <- res[[i]]
+    attr(x, "id") <- as.character(liid[[i]][1])
+    attr(x, "burst") <- levels(factor(burst))[i]
+    return(x)
+  })
+  if (!is.null(infolocs)) {
+    res <- lapply(1:length(res), function(i) {
+      x <- res[[i]]
+      y <- linfol[[i]]
+      row.names(y) <- row.names(x)
+      attr(x, "infolocs") <- y
+      return(x)
+    })
+  }
+  class(res) <- c("ltraj", "list")
+  attr(res, "typeII") <- typeII
+  attr(res, "regular") <- is.regular(res)
+  return(res)
+}
+assignInNamespace('as.ltraj', my.as.ltraj, ns = 'adehabitatLT')
+RediscretizedTrack <- as.ltraj(xy = Track[,c('Longitude', 'Latitude')],
+         date = Track$UTC,
+         id = Track$DeployID,
+         burst = paste(Track$DeployID, Track$TripID, sep = '-')) %>%
+  redisltraj(u = 180, 
+             samplex0 = TRUE, 
+             type = 'time')
+
+Track <- mutate(Track, UTC = as.numeric(UTC))  
 
 # Test primary keys
 pkeys <- list(DeploymentMetadata = 'DeployID',
