dc74e43afef51393332a43451c1b88a9ca0739dc
Max Czapanskiy <mczapanskiy@usgs.gov>
2016-01-21 12:01:27 -0800

    Find ARS zones by 95 percentile FPT points (radius per trip)

--- src/ars-analysis.R
+++ src/ars-analysis.R
@@ -15,7 +15,7 @@ trips <- dbGetQuery(MHIdb,
 "SELECT T.DeployID, T.TripID, T.Begin, T.End, T.Duration,
   BM.FieldID, BM.Species
 FROM Trip T
-  JOIN DeploymentMEtadata DM
+  JOIN DeploymentMetadata DM
     ON DM.DeployID = T.DeployID
   JOIN BirdMetadata BM
     ON BM.DeployID = T.DeployID
@@ -32,7 +32,100 @@ trips <- trips %>%
   filter(!EXCLUDE) %>%
   dplyr::select(-EXCLUDE)
 
-foreach(trip = iter(trips, by = 'row')) %do% {
+# 295 trips
+ntrips <- nrow(trips)
+
+smoother <- function(vec, fil) {
+  wing <- (length(fil) - 1) / 2
+  vec2 <- c(rep(vec[1], wing),
+            vec,
+            rep(vec[length(vec)], wing))
+  fil2 <- fil / sqrt(sum(fil)^2)
+  convolve(vec2, fil2, type = 'filter')
+}
+
+ars_scale_file <- 'Hawaii_data/Lehua/ars/scales.csv'
+ars_scales <- if(file.exists(ars_scale_file)) {
+  read.csv(ars_scale_file)
+} else {
+  manual_results <- foreach(trip = iter(trips, by = 'row'), i = ntrips) %do% {
+    track <- dbGetPreparedQuery(MHIdb,
+                                "SELECT *
+                                FROM RediscretizedTrack T
+                                WHERE T.DeployID = ?
+                                AND T.TripID = ?",
+                                dplyr::select(trip, DeployID, TripID))
+    
+    x0 <- mean(track$Longitude)
+    y0 <- mean(track$Latitude)
+    origin = c(x0, y0)
+    
+    trackxy <- mutate(track,
+                      UTC = POSIXct.from.db(UTC),
+                      d = distGeo(origin, cbind(Longitude, Latitude)),
+                      b = bearing(cbind(Longitude, Latitude), origin),
+                      x = d * -sin(b * pi / 180),
+                      y = d * -cos(b * pi / 180),
+                      step = sqrt((lead(x) - x)^2 + (lead(y) - y)^2))
+    
+    tracklt <- with(trackxy, as.ltraj(cbind(x, y), 
+                                      date = UTC, 
+                                      id = paste(trip$DeployID, trip$TripID, sep = '_')))
+    fptresults <- fpt(tracklt, radii = seq(from = 1e3, to = 500e3, by = .5e3), units = 'seconds')
+    
+    rv <- data.frame(radius = attr(fptresults, 'radii'),
+                     varlog = apply(fptresults[[1]], 2, function(f) var(log(f), na.rm = TRUE))) %>%
+      na.omit
+    
+    rv <- rv %>%
+      mutate(varlog2 = smoother(varlog, c(1, 2, 4, 8, 4, 2, 1)),
+             dv_dr = (lead(varlog2) - varlog2) / (lead(radius) - radius),
+             d2v_dr2 = (lead(dv_dr) - dv_dr) / (lead(radius) - radius))
+    
+    x11(1200,800, title = as.character(i))
+    plot(varlog ~ radius, rv)
+    lines(varlog2 ~ radius, rv)
+    ars_radii <- rv$radius[with(rv, identify(radius, varlog, labels = radius))]
+    dev.off()
+    
+    list(DeployID = trip$DeployID, 
+         TripID = trip$TripID,
+         Nfpt = nrow(rv),
+         ARS_radii = ars_radii)
+  } %>%
+    lapply(function(trip) {
+      data.frame(DeployID = trip$DeployID, 
+                 TripID = trip$TripID, 
+                 Nfpt = trip$Nfpt, 
+                 ARS_radii = if(length(trip$ARS_radii) == 0) NA else trip$ARS_radii)
+    }) %>%
+    rbindlist
+  write.csv(ars_scales_manual, 'Hawaii_data/Lehua/ars/scales.csv', row.names = FALSE)
+}
+
+metadata <- dbGetQuery(MHIdb,
+"SELECT BM.DeployID, BM.Species
+FROM BirdMetadata BM 
+WHERE BM.Species IN ('BRBO', 'RFBO')") %>%
+  as.data.table
+
+ars_scales <- left_join(ars_scales, metadata) %>%
+  as.data.frame
+
+ars_scales %>%
+  ggplot(aes(ARS_radii,
+             color = Species)) +
+  geom_density(size = 1) +
+  ggtitle('Foraging scale distribution across species')
+
+ars_scales %>%
+  group_by(Species) %>%
+  summarize(mean_radius = mean(ARS_radii, na.rm=TRUE),
+            median_radius = median(ARS_radii, na.rm = TRUE),
+            sd_radius = sd(ARS_radii, na.rm = TRUE))
+
+ARS_zones <- foreach(trip = iter(ars_scales, by = 'row'), .combine = rbind) %do% {
+  if(is.na(trip$ARS_radii)) return(NULL)
   track <- dbGetPreparedQuery(MHIdb,
                               "SELECT *
                               FROM RediscretizedTrack T
@@ -55,42 +148,48 @@ foreach(trip = iter(trips, by = 'row')) %do% {
   tracklt <- with(trackxy, as.ltraj(cbind(x, y), 
                                     date = UTC, 
                                     id = paste(trip$DeployID, trip$TripID, sep = '_')))
-  fptresults <- fpt(tracklt, radii = seq(from = 1e3, to = 500e3, by = 1e3), units = 'seconds')
   
-  rv <- data.frame(radius = attr(fptresults, 'radii'),
-                   variance = apply(fptresults[[1]], 2, function(f) var(log(f), na.rm = TRUE))) %>%
-    na.omit
-  
-  smoother <- function(vec, fil) {
-    wing <- (length(fil) - 1) / 2
-    vec2 <- c(rep(vec[1], wing),
-              vec,
-              rep(vec[length(vec)], wing))
-    fil2 <- fil / sqrt(sum(fil)^2)
-    convolve(vec2, fil2, type = 'filter')
-  }
-  
-  rv <- rv %>%
-    mutate(log_var = log(variance),
-           log_var_smooth = smoother(log_var, c(2,3,5,3,2)),
-           dv_dr = (lead(log_var_smooth) - log_var_smooth) / (lead(radius) - radius),
-           d2v_dr2 = (lead(dv_dr) - dv_dr) / (lead(radius) - radius))
-  
-  x11()
-  plot(log_var ~ radius, filter(rv, log_var >= -10))
-  lines(log_var_smooth ~ radius, rv)
-  ars_radii <- rv$radius[with(rv, identify(radius, log_var, labels = radius))]
-  dev.off()
+  trackxy %>% 
+    mutate(fpt = fpt(tracklt, radii = trip$ARS_radii, units = 'seconds')[[1]]$r1) %>%
+    filter(percent_rank(fpt) >= .95) %>%
+    transmute(DeployID,
+              TripID,
+              Longitude,
+              Latitude,
+              FPT = fpt,
+              Radius = trip$ARS_radii)
+}
+write.csv(ARS_zones, 'Hawaii_data/Lehua/ars/zones.csv', row.names = FALSE)
 
-  list(DeployID = trip$DeployID, 
-       TripID = trip$TripID,
-       Nfpt = nrow(rv),
-       ARS_radii = ars_radii)
-} %>%
-  lapply(function(trip) {
-    data.frame(DeployID = trip$DeployID, 
-               TripID = trip$TripID, 
-               Nfpt = trip$Nfpt, 
-               ARS_radii = if(length(trip$ARS_radii) == 0) NA else trip$ARS_radii)
-  }) %>%
-  rbindlist -> ars_scales
+FPT_TRACKS <- foreach(trip = iter(ars_scales, by = 'row'), .combine = rbind) %do% {
+  if(is.na(trip$ARS_radii)) return(NULL)
+  track <- dbGetPreparedQuery(MHIdb,
+                              "SELECT *
+                              FROM RediscretizedTrack T
+                              WHERE T.DeployID = ?
+                              AND T.TripID = ?",
+                              dplyr::select(trip, DeployID, TripID))
+  
+  x0 <- mean(track$Longitude)
+  y0 <- mean(track$Latitude)
+  origin = c(x0, y0)
+  
+  trackxy <- mutate(track,
+                    UTC = POSIXct.from.db(UTC),
+                    d = distGeo(origin, cbind(Longitude, Latitude)),
+                    b = bearing(cbind(Longitude, Latitude), origin),
+                    x = d * -sin(b * pi / 180),
+                    y = d * -cos(b * pi / 180),
+                    step = sqrt((lead(x) - x)^2 + (lead(y) - y)^2))
+  
+  tracklt <- with(trackxy, as.ltraj(cbind(x, y), 
+                                    date = UTC, 
+                                    id = paste(trip$DeployID, trip$TripID, sep = '_')))
+  
+  trackxy %>% 
+    mutate(fpt = fpt(tracklt, radii = trip$ARS_radii, units = 'seconds')[[1]]$r1,
+           radius = trip$ARS_radii,
+           DTRid = paste(DeployID, TripID, radius, sep = "_"))
+}
+
+write.csv(FPT_TRACKS, 'Hawaii_data/Lehua/ars/fpttracks.csv', row.names = FALSE, na = "")
