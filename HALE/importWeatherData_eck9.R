cb34ff35adada26b5475bec444b0bf207e445635
Emma <ekelsey@usgs.gov>
2016-06-22 17:12:55 -0700

    started weather import script

--- HALE/importWeatherData_eck9.R
+++ HALE/importWeatherData_eck9.R
@@ -1,2 +1,15 @@
-%% import and organize HALEnet files
+## import and organize HALEnet files
 
+library(tidyr)
+library(dplyr)
+setwd("~/PredControl/HaleNetFiles")
+
+rainfall <- read.table("HaleNet_Raifall_Daily", header=TRUE) %>%
+  gather(key=date, value=rainfall, -(parameter:elev)) %>%
+  mutate(date=as.Date(date, format="X%Y.%m.%d"))
+  
+relHumidity <- read.table("HaleNet_RelativeHumidity_Daily", header=TRUE) %>%
+  gather(key=date, value=relHumidity, -(parameter:elev)) %>%
+  mutate(date=as.Date(date, format="X%Y.%m.%d"))
+
+weather <- merge(rainfall, relHumidity)
