e5fc124daf61827d3b18e076d942fb92ada99ea8
Max Czapanskiy <mczapanskiy@usgs.gov>
2016-06-27 13:48:05 -0700

    trying to debug cdf issue
    manually generated poisson distribution works fine. what is it about
    shearwater data that pukes?

--- sh_gamlss/gamlss-test.R
+++ sh_gamlss/gamlss-test.R
@@ -1,6 +1,4 @@
 library(gamlss)
-library(pscl)
-library(MASS)
 library(dplyr)
 library(ggplot2)
 
@@ -13,6 +11,10 @@ sh.data <- read.csv('sh_gamlss/TelemetryTransect17May2016_SOSH-PFSH-COMU.csv') %
 head(sh.data)
 summary(sh.data)
 
+sh.data2 <- slice(sh.data, 1:100) %>%
+  select(SOSHcount, Dist200)
+gamlss(SOSHcount ~ Dist200, data = sh.data2, family = ZIP)
+
 # Test model: SOSHcount as function of FCPI, Dist200, SSTmean (all three cubic splines) with Month as random variable 
 # and log(Binarea) as offset
 gamlssTest <- gamlss(SOSHcount ~ FCPI,
@@ -21,18 +23,14 @@ gamlssTest <- gamlss(SOSHcount ~ FCPI,
                      data = sh.data, 
                      family = NBI)
 
-d <- data.frame(x <- runif(100)) %>%
-  mutate(y = sin(x * 2*pi) + runif(100))
-gamlssTest <- gamlss(y ~ cs(x), data = d)
-ggplot(mutate(d, y2 = predict(gamlssTest)),
-       aes(x, y)) +
-  geom_point() + 
-  geom_line(aes(x, y2, group = 1))
+rdist <- Vectorize(function(x) {
+  result <- 0
+  while(runif(1) <= x) result <- result + 1
+  return(result)
+})
+d <- data.frame(x = runif(100) * .5) %>%
+  mutate(y = rdist(x))
 
-d <- data.frame(x = runif(100)) %>%
-  mutate(y = rpois(100, x))
 gamlssTest <- gamlss(y ~ x, data = d, family = PO)
-ggplot(mutate(d, y2 = predict(gamlssTest)), 
-       aes(x, y)) +
-  geom_point() +
-  geom_line(aes(x, y2, group = 1))
+
+View(sh.data2)
