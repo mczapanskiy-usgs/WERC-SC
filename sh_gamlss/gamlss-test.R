36c5e4e91d9ec9b3caa13604f0fd70876a7b039d
Max Czapanskiy <mczapanskiy@usgs.gov>
2016-06-29 10:21:16 -0700

    cdf bug fix
    SOSHcount data was being read as double. So 1 was actually .999999. cdf
    runs on y-1 and needs positive values, so 1s became -.00000001. Use
    mutate to convert SOSHcount to integers when reading data.

--- sh_gamlss/gamlss-test.R
+++ sh_gamlss/gamlss-test.R
@@ -5,41 +5,19 @@ library(dplyr)
 sosh.data <- read.csv('sh_gamlss/TelemetryTransect17May2016_SOSH-PFSH-COMU.csv') %>%
   dplyr::select(SOSHcount, Binarea, Month, FCPI, Dist200, SSTmean) %>%
   filter(Month > 2) %>% # exclude tiny January, February surveys
-  mutate(Month = factor(Month)) %>%
+  mutate(SOSHcount = as.integer(SOSHcount),
+         Month = factor(Month)) %>%
   na.omit
 head(sosh.data)
 summary(sosh.data)
 
-pfsh.data <- read.csv('sh_gamlss/TelemetryTransect17May2016_SOSH-PFSH-COMU.csv') %>%
-  dplyr::select(PFSHcount, Binarea, Month, FCPI, Dist200, SSTmean) %>%
-  filter(Month > 2) %>% # exclude tiny January, February surveys
-  mutate(Month = factor(Month)) %>%
-  na.omit
-head(pfsh.data)
-summary(pfsh.data)
-
-sosh.gamlss <- gamlss(SOSHcount ~ FCPI +
-                        offset(log(Binarea)) + random(Month) + 
-                        cs(FCPI) + cs(Dist200) + cs(SSTmean),
-                      data = sosh.data,
-                      family = NBI)
-
 # Test model: SOSHcount as function of FCPI, Dist200, SSTmean (all three cubic splines) with Month as random variable 
 # and log(Binarea) as offset
-gamlssTest <- gamlss(SOSHcount ~ FCPI,
-#                        offset(log(Binarea)) + random(Month) + 
-#                        cs(FCPI) + cs(Dist200) + cs(SSTmean), 
+gamlssTest <- gamlss(SOSHcount ~ cs(FCPI) + cs(Dist200) + cs(SSTmean) +
+                       offset(log(Binarea)) + random(Month),
                      data = sosh.data, 
                      family = NBI)
 
-rdist <- Vectorize(function(x) {
-  result <- 0
-  while(runif(1) <= x) result <- result + 1
-  return(result)
-})
-d <- data.frame(x = runif(100) * .5) %>%
-  mutate(y = rdist(x))
-
-gamlssTest <- gamlss(y ~ x, data = d, family = PO)
-
-View(sosh.data2)
+summary(gamlssTest)
+plot(gamlssTest)
+wp(gamlssTest)
