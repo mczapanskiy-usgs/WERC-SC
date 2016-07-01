af9be5fd9693608f2a8a6a6d39fb0520c4761a5b
Emma <ekelsey@usgs.gov>
2016-07-01 13:34:33 -0700

    more updates to the eventTypePUE script

--- HALE/HALE_CPUE_eventTypePUE_eck11.R
+++ HALE/HALE_CPUE_eventTypePUE_eck11.R
@@ -9,24 +9,23 @@ library(lubridate)
 read.csv('~/WERC-SC/HALE/catch_traploc_weeks_baitTypes_edited_predEvent.csv',
          stringsAsFactors = FALSE) -> catch
 
-## add variable IDing the check week for each trapline (Jan 1-7, 2000 = week 1...)
+## add variable IDing the check week so all trapsin a given week are group together by trapline (Jan 1-7, 2000 = week 1...)
 # also rank predEvent by importance (catCaught > mongooseCaught > ratCaught > mouseCaught > birdOtherCaught > baitStolen > trapTriggered > none)
 catch <- mutate(catch, 
                 date = mdy(date), # change data from character to POSIXct
-                week = as.numeric(as.period(interval(min(date), date) %/% weeks(1))),
+                week = as.numeric(as.period(interval(min(date), date) %/% weeks(1))), #lubridate function
                 predEvent = factor(predEvent, level = c('catCaught', 'mongooseCaught', 'ratCaught', 'mouseCaught', 'birdOtherCaught', 'baitStolen', 'trapTriggered', 'none'), ordered = TRUE)) 
 
-## count the number of traps in a trapline each week
-# if there are multiple predEvents in a week, choose the most important one 
+## if there are multiple predEvents in a week, choose the most important one 
 weeklyCatches <- catch %>%
   group_by(Trapline, TrapNum, week) %>% 
   summarize(predEvent = min(predEvent))
-# then summarize the number of Trapline events per week (aka number of traps in the trapline)
+
+## count the number of Trapline events (weeklyCatches) per week (aka number of traps in the trapline)
 TrapsPerLineWeek <- weeklyCatches %>%
   group_by(Trapline, week) %>%
   summarize(NTraps = n())
-
-# count number of trap events per week per trapline
+# count number of each predEvent per week per trapline
 PredEventsPerLineWeek <- weeklyCatches %>% 
   group_by(Trapline, week, predEvent) %>% 
   summarize(NEvents = n())
@@ -36,7 +35,7 @@ PredEventPUE <- merge(TrapsPerLineWeek, PredEventsPerLineWeek) %>%
   mutate(CPUE = NEvents/NTraps) %>% 
   arrange(Trapline, week, predEvent)
 
-## ID how many times a trap was checked multiple times in a week (and thus only the most important trap event was chosen)
+## data validation: ID how many times a trap was checked multiple times in a week (and thus only the most important trap event was chosen)
 uniqueTrapsPerWeek <- catch %>% 
   group_by(Trapline, week, TrapNum) %>% 
   summarize(N = n()) %>% 
