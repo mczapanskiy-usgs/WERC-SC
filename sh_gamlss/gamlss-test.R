22fa71aa78c2598db599c5b07a8d15adb150ccd4
Max Czapanskiy <mczapanskiy@usgs.gov>
2016-07-12 14:53:59 -0700

    Monthly geographic and oceanographic models

--- sh_gamlss/gamlss-test.R
+++ sh_gamlss/gamlss-test.R
@@ -1,9 +1,13 @@
 library(gamlss)
 library(dplyr)
+library(foreach)
 
 # Read data
 sosh.data <- read.csv('sh_gamlss/TelemetryTransect17May2016_SOSH-PFSH-COMU.csv') %>% 
-  dplyr::select(SOSHcount, Binarea, Month, FCPI, Dist200, SSTmean) %>%
+  dplyr::select(SOSHcount, Binarea, Month, 
+                Latitude, DistCoast, Dist200, DepCI,
+                SSTmean, STD_SST, MEAN_Beaufort, L10CHLproxy, STD_CHL_log10, Watermass,
+                L10CHLsurvey, CHLsurvanom, L10CHLsurvclim, FCPI) %>%
   filter(Month > 2) %>% # exclude tiny January, February surveys
   mutate(SOSHcount = as.integer(SOSHcount),
          Month = factor(Month)) %>%
@@ -11,37 +15,63 @@ sosh.data <- read.csv('sh_gamlss/TelemetryTransect17May2016_SOSH-PFSH-COMU.csv')
 head(sosh.data)
 summary(sosh.data)
 
-# Create models using canonical discrete distributions.
-# SOSHcount as function of FCPI, Dist200, SSTmean (all three cubic splines) with Month as random variable 
-# and log(Binarea) as offset
+# Create models using canonical discrete distributions for each month.
+# Geographic + oceanographic models
+# geo = SOSHcount ~  cs(Latitude)+cs(DistCoast)+cs(Dist200)+cs(DepCI) + offset(log(Binarea))
+# ocean = SOSHcount ~ cs(SSTmean)+cs(STD_SST)+cs(MEAN_Beaufort)+cs(L10CHLproxy)+cs(STD_CHL_log10)+Watermass+cs(L10CHLsurvey)+
+#   cs(CHLsurvanom)+cs(L10CHLsurvclim)+cs(FCPI) + offset(log(Binarea))
 discrete.dist <- c('PO', 'NBI', 'NBII', 'DEL', 'PIG', 'SI', 'SICHEL', 'ZIP', 'ZIP2')
-all.models <- lapply(discrete.dist, function(dist) {
-  result <- gamlss(SOSHcount ~ cs(FCPI) + cs(Dist200) + cs(SSTmean) +
-                     offset(log(Binarea)) + random(Month),
-                   data = sosh.data, 
-                   family = get('dist'))
-  if(!getElement(result, 'converged')) try(result <- refit(result))
-  result
-})
+months <- c(6, 7, 9, 10)
+# For each discrete distribution...
+monthly.geo.ocean.models <- foreach(dist = discrete.dist) %do% {
+  # For each month's survey...
+  foreach(month = months) %do% {
+    print(sprintf('Distribution %s, month %i', dist, month))
+    
+    # Filter SOSH data to the month of interest
+    month.data <- filter(sosh.data, Month == month)
+    if(nrow(month.data) == 0) stop(sprintf('No data in month %i', month))
+    
+    # Fit geographic model
+    print('Fitting geographic model')
+    geo.model <- try(gamlss(SOSHcount ~ cs(Latitude) + cs(DistCoast) + cs(Dist200) + cs(DepCI) + offset(log(Binarea)),
+                            data = month.data,
+                            family = get('dist')))
+    # Try one round of refitting if it doesn't converge
+    if(class(geo.model) != 'try-error' && !getElement(geo.model, 'converged')) {
+      print('Refitting geographic model')
+      try(geo.model <- refit(geo.model))
+    }
+    
+    # Fit oceanographic model
+    print('Fitting oceanographic model')
+    ocean.model <- try(gamlss(SOSHcount ~ cs(SSTmean) + cs(STD_SST) + cs(MEAN_Beaufort) + cs(L10CHLproxy) + 
+                                cs(STD_CHL_log10) + Watermass + cs(L10CHLsurvey) + cs(CHLsurvanom) + 
+                                cs(L10CHLsurvclim) + cs(FCPI) + offset(log(Binarea)),
+                              data = month.data,
+                              family = get('dist')))
+    # Try one round of refitting if it doesn't converge
+    if(class(ocean.model) != 'try-error' && !getElement(ocean.model, 'converged')) {
+      print('Refitting oceanographic model')
+      try(ocean.model <- refit(ocean.model))
+    }
+    
+    list(dist = dist,
+         month = month,
+         geo.model = geo.model,
+         ocean.model = ocean.model)
+  }
+}
 
-model.rankings <- sapply(all.models, function(model) c(family = model$family[1], # Distribution used
-                                                    df = extractAIC(model)[1], # degrees of freedom
-                                                    AIC = extractAIC(model)[2], # AIC value
-                                                    converged = getElement(model, 'converged'))) %>% # whether model converged
-  t %>%
-  as.data.frame %>%
-  arrange(AIC)
-
-# Create models across surveys
-# Use ZIP, which had the lowest AIC across all months
-indiv.months <- lapply(unique(sosh.data$Month), function(month) filter(sosh.data, Month == month)) %>%
-  lapply(function(month) {
-    gamlss(SOSHcount ~ cs(FCPI) + cs(Dist200) + cs(SSTmean) +
-             offset(log(Binarea)),
-           data = sosh.data, 
-           family = ZIP)
-  })
-
-summary(gamlssTest)
-plot(gamlssTest)
-wp(gamlssTest)
+model.rankings <- foreach(dist.list = monthly.geo.ocean.models, .combine = rbind) %do% {
+  foreach(models = dist.list, .combine = rbind) %do% {
+    data.frame(Distribution = models$dist,
+               Month = models$month,
+               GeoConverged = tryCatch(getElement(models$geo.model, 'converged'), error = function(e) NA),
+               GeoEDF = tryCatch(extractAIC(models$geo.model)[1], error = function(e) NA),
+               GeoAIC = tryCatch(extractAIC(models$geo.model)[2], error = function(e) NA),
+               OceanConverged = tryCatch(getElement(models$ocean.model, 'converged'), error = function(e) NA),
+               OceanEDF = tryCatch(extractAIC(models$ocean.model)[1], error = function(e) NA),
+               OceanAIC = tryCatch(extractAIC(models$ocean.model)[2], error = function(e) NA))
+  }
+}
