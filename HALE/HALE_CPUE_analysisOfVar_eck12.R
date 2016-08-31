3f6ff1fd589a321445a859f89bda4498d6ecfe1d
Emma <ekelsey@usgs.gov>
2016-08-31 16:51:46 -0700

    combined eck11 and seasonal eck12 into one script
    also fixed bug in lubridate funtion in eck11

--- HALE/HALE_CPUE_analysisOfVar_eck12.R
+++ HALE/HALE_CPUE_analysisOfVar_eck12.R
@@ -7,18 +7,25 @@ library("nnet", lib.loc="C:/Program Files/R/R-3.2.3/library")
 library(ggplot2)
 library(Rcpp)
 
-read.csv('~/WERC-SC/HALE/TraplineWeeklyCatches.csv',
-         stringsAsFactors = FALSE) -> weeklyCatches
+# read.csv('~/WERC-SC/HALE/TraplineWeeklyCatches.csv',
+#          stringsAsFactors = FALSE) -> weeklyCatches
+# 
+# ## get a feel for the data
+# with(weeklyCatches, table(predEvent, Year))
+# with(weeklyCatches, table(predEvent, baitType))
+# 
+# mutate(weeklyCatches, catch = (predEvent %in% c('catCaught', 'ratCaught', 'mongooseCaught', 'mouseCaught'))) -> weeklyCatches
+# 
+# catchGLM <- lm(catch ~ Year + Month, data = weeklyCatches)
+# summary(catchGLM)
+# anova(catchGLM)
+# 
+# mutate(weeklySeasonalCatches, catch = (predEvent %in% c('catCaught', 'ratCaught', 'mongooseCaught', 'mouseCaught'))) -> weeklySeasonalCatches
+# 
+# seasonCatchGLM <- lm(catch ~ Year + season, data = weeklySeasonalCatches)
+# summary(seasonCatchGLM)
+# anova(seasonCatchGLM)
 
-## get a feel for the data
-with(weeklyCatches, table(predEvent, Year))
-with(weeklyCatches, table(predEvent, baitType))
-
-mutate(weeklyCatches, catch = (predEvent %in% c('catCaught', 'ratCaught', 'mongooseCaught', 'mouseCaught'))) -> weeklyCatches
-
-catchGLM <- lm(catch ~ baitType + Year + Month + Trapline, data = weeklyCatches)
-summary(catchGLM)
-anova(catchGLM)
 # ## convert data to the correct data types
 # weeklyCatches$Year = as.numeric(weeklyCatches$Year)
 # weeklyCatches$week = as.numeric(weeklyCatches$week)
--- HALE/HALE_CPUE_eventTypePUE_eck11.R
+++ HALE/HALE_CPUE_eventTypePUE_eck11.R
@@ -4,58 +4,65 @@
 library("data.table", lib.loc="~/R/win-library/3.2")
 library("dplyr", lib.loc="~/R/win-library/3.2")
 library(lubridate)
+library(mosaic)
 
 ## read in file
 read.csv('~/WERC-SC/HALE/catch_traploc_weeks_baitTypes_edited_predEvent.csv',
-         stringsAsFactors = FALSE) -> catch
-
+         stringsAsFactors = FALSE) -> raw_catch
+catch <- mutate(raw_catch, 
+                date = mdy(date), 
+                Week = interval(min(date), date))
 ## add variable IDing the check week so all trapsin a given week are group together by trapline (Jan 1-7, 2000 = week 1...)
 # also rank predEvent by importance (catCaught > mongooseCaught > ratCaught > mouseCaught > birdOtherCaught > baitLost > trapTriggered > none)
-catch <- mutate(catch, 
+catch <- mutate(raw_catch, 
                 date = mdy(date), # change data from character to POSIXct
-                week = as.numeric(as.period(interval(min(date), date) %/% weeks(1))), #lubridate function
+                Week = as.numeric(as.period(lubridate::interval(min(date), date) %/% weeks(1))), #lubridate function
                 predEvent = factor(predEvent, 
                                    level = c('catCaught', 'mongooseCaught', 'ratCaught', 'mouseCaught', 'birdOtherCaught', 'baitLost', 'trapTriggered', 'none'), 
-                                   ordered = TRUE)) 
+                                   ordered = TRUE), 
+                Season = derivedFactor(
+                          "offSeason" = Month == 1,
+                          "Pre-laying" = Month >= 2,
+                          "Incubation" = Month >= 5,
+                          "Nestling" = Month >= 7,
+                          "offSeason" = Month >= 11,
+                          .method = "last", 
+                          .default = "offSeason"))
 
 ## if there are multiple predEvents in a week, choose the most important one 
 weeklyCatches <- catch %>%
-  group_by(Trapline, TrapNum, Year, Month, week, baitType) %>% 
+  group_by(Trapline, TrapNum, Season, Year, Month, Week) %>% 
   summarize(predEvent = min(predEvent))
 
 ## count the number of Trapline events (weeklyCatches) per week (aka number of traps in the trapline)
 trapsPerLineWeek <- weeklyCatches %>%
-  group_by(Trapline, Year, week) %>%
+  group_by(Trapline, Season, Year, Month, Week) %>%
   summarize(NTraps = n())
 
 # count number of each predEvent per week per trapline
 predEventsPerLineWeek <- weeklyCatches %>% 
-  group_by(Trapline, week, predEvent, baitType) %>% 
+  group_by(Trapline, Season, Month, Week, predEvent) %>% 
   summarize(NEvents = n())
 
 ## number of predEvents per number of traps, for each week on each Trapline
 predEventPUE <- merge(trapsPerLineWeek, predEventsPerLineWeek) %>% 
   mutate(CPUE = NEvents/NTraps) %>% 
-  arrange(Trapline, Year, week, baitType, predEvent)
+  arrange(Trapline, Season, Year, Month, Week, predEvent)
 
 ## data validation: ID how many times a trap was checked multiple times in a week (and thus only the most important trap event was chosen)
 uniqueTrapsPerWeek <- catch %>% 
   filter(!TrapChecked) # first remove dates when trap hadn't been checked in >14 days
-  group_by(Trapline, week, TrapNum) %>% 
+  group_by(Trapline, Week, TrapNum) %>% 
   summarize(N = n()) %>% 
   filter(N > 1) %>% 
   arrange(-N)
 
-  
 ## save predEventPUE data file to GitHub file
-# weekly catch for each trap
-write.csv(weeklyCatches, file = '~/WERC-SC/HALE/TraplineWeeklyCatches.csv',
-            row.names = FALSE)
-  
-# Weekly predEventPUE per Trapline
 write.csv(predEventPUE, file = '~/WERC-SC/HALE/TraplinePredEventPUE.csv',
             row.names = FALSE) 
-  
+# # weekly catch for each trap
+# write.csv(weeklyCatches, file = '~/WERC-SC/HALE/TraplineWeeklyCatches.csv',
+#           row.names = FALSE) 
   
 ### summary stats and graphs of predEventPUE
 # frequency of predEvents per trapline per year
@@ -81,6 +88,45 @@ preds <- ggplot(traplineCPUE, aes(Year, annualFreq, color=predEvent)) +
   theme(axis.text.x = element_text(angle=60, hjust=1)) +
   ylim(0, 0.4)
 preds %+% subset(traplineCPUE, predEvent %in% c("catCaught", "mongooseCaught", "ratCaught", "mouseCaught"))
+
+## seasonal trends
+# frequency of predEvents per trapline per season
+seasonalTraplineCPUE <- predEventPUE %>%
+  group_by(Trapline, Year, season, predEvent) %>%
+  summarize(seasonalFreq = mean(CPUE)) ## does this make sense?
+# frequency of predEvents per trapline per month
+monthlyTraplineCPUE <- predEventPUE %>%
+  group_by(Trapline, Year, Month, predEvent) %>%
+  summarize(monthlyFreq = mean(CPUE)) ## does this make sense?
+
+## plot of predEvents per trapline per month
+ggplot(monthlyTraplineCPUE, aes(Month, monthlyFreq, color=Year)) + # , group = Year)) +
+  geom_point() + # geom_line() +
+  scale_x_continuous(breaks = c(1,2,3,4,5,6,7,8,9,10,11,12)) +
+  labs(x = 'Month', y = 'Monthly Frequency') +
+  facet_wrap(~ predEvent, nrow = 4) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle=60, hjust=1))
+# predEvents per trapline per month, for just predators
+monthlyPreds <- ggplot(monthlyTraplineCPUE, aes(Month, monthlyFreq)) +
+  geom_point() +
+  scale_x_continuous(breaks = c(1,2,3,4,5,6,7,8,9,10,11,12)) + # theme(axis.text.x = element_text(angle=60, hjust=1))
+  labs(x = 'Month', y = 'Monthly Frequency') +
+  facet_wrap(~ predEvent, nrow = 4) +
+  theme_bw() 
+monthlyPreds %+% subset(monthlyTraplineCPUE, predEvent %in% c("catCaught", "mongooseCaught", "ratCaught", "mouseCaught"))
+
+## plot of predEvents per trapline per season
+seasonalPreds <- ggplot(TraplineCPUE, aes(season, seasonalFreq, color=Year, group = Year)) +
+  geom_point() +
+  geom_line() +
+  labs(x = 'Season', y = 'Seasonal Frequency') +
+  facet_wrap(~ predEvent, nrow = 5) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle=60, hjust=1))
+
+# # predEvents per trapline per season, for just predators
+# seasonalPreds %+% subset(seasonalTraplineCPUE, predEvent %in% c("catCaught", "mongooseCaught", "ratCaught", "mouseCaught"))
 
 # 
 # # histogram of pred events per trapline
