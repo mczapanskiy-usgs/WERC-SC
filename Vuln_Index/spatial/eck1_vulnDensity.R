7f984691488df1d41703fb665cabc63f38cd4cf1
Emma <ekelsey@usgs.gov>
2016-10-28 16:44:34 -0700

    more work on the density/vulnerability function

--- Vuln_Index/spatial/eck1_vulnDensity.R
+++ Vuln_Index/spatial/eck1_vulnDensity.R
@@ -7,26 +7,26 @@ setwd("~/WERC-SC/Vuln_Index/spatial")
 
 library("data.table", lib.loc="~/R/win-library/3.2")
 library("dplyr", lib.loc="~/R/win-library/3.2")
+library(tidyr)
+
 
 # read in files
 read.csv('~/WERC-SC/Vuln_Index/spatial/SoCAvulnScores.csv',
          stringsAsFactors = FALSE) -> vulnScores
 read.csv('~/WERC-SC/Vuln_Index/spatial/SoCA5minDensities.csv',
          stringsAsFactors = FALSE) -> densities
+densities <- select(densities, -ALL_BIRDS)
 
 # vuln score percent rank
 vulnRanks <- mutate(vulnScores,
                     PCVrank = percent_rank(PCV), 
                     PDVrank = percent_rank(PDV))
 
-# density percent rank
-densityRanks <- mutate_each(densities,
-                         funs(percent_rank))        #, matches("^[A-Z]\\."), ignore.case=FALSE
+# density percent rank and merge with vulnRanks
+densities_long <- gather(densities, species, dens, ASSP:XAMU) %>% # change to long format (tidyr)
+  mutate(densityRank = percent_rank(dens)) %>% # add percent rank of all survey densities
+  merge(vulnRanks, by.x = "species", by.y = "SurveySpp") %>%  # merge with vulnRanks table
+  mutate(rankPCV = densityRank*PCVrank,
+         rankPDV = densityRank*PDVrank)
 
-# rank <- names(vulnRanks$SurveySpp)[1:70]
-# rank <- setNames(rank, paste0(rank, "_rank"))
-
-# write for loop to apply percent rank to all species densities
-for (spp in vulnRanks$SurveySpp) {
-  mutate(densityRanks, spp = funs(.*spp))
-}
+densityVulnRanks <- spread(densities_long, species, TGRIDALB_I)
