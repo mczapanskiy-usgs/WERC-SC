ce62ee5755bd3e3653c2cd81cf694ed54e9dd755
Emma <ekelsey@usgs.gov>
2016-11-15 16:48:41 -0800

    new visualization of vulnerability data
    trying to figure out convex hull analysis

--- Vuln_Index/eck8.vulnSummaryScatterPlots_updated.R
+++ Vuln_Index/eck8.vulnSummaryScatterPlots_updated.R
@@ -4,7 +4,6 @@
 setwd("~/WERC-SC/Vuln_Index")
 
 ## load data and toolboxes
-
 PVscores <- read.csv("PCV&PDVscores.csv") ## matrix of cumulative PV, CPV, and DPV before 1-10 ranking
 PVscores <- PVscores[complete.cases(PVscores), ] # remove blank observations at bottom of matrix
 scores <- read.csv("PV.CV.DVscores.csv") ## matrix of final PV, CV, and DV
@@ -31,69 +30,64 @@ x1 <- ggplot(PVscores,
   scale_x_continuous(name="Population Displacement Vulnerability", limits=c(5,200)) +
   scale_y_continuous(name="Population Collision Vulnerability", limits=c(5,300)) +
   theme_bw(base_size = 14) + 
-  scale_colour_manual(values=cbbPalette) + # scale_colour_brewer(palette = "Set1") 
+  scale_colour_manual(values=cbbPalette) # + scale_colour_brewer(palette = "Set1") 
 x1
 ggsave("PCVvsPDV.pdf")
-  
+
 ## graph CV vs DV colored by species group with PV scaling points
 scores$AlphaCode <- factor(scores$AlphaCode, levels = scores$AlphaCode[order(scores$Order)]) # will keep original order of species
 x2 <- ggplot(scores, 
-             aes(DispBest, 
-                 ColBest, 
-                 size=PopBest)) + 
-  geom_point(aes(colour = factor(Taxomony)), 
-            size=3, 
-            face="bold") +
+             aes(ColBest,
+                 DispBest, 
+                 size = PopBest, 
+                 color = Taxonomy)) + 
+  geom_point() + # (aes(colour = factor(Taxomony)), size=3,face="bold") +
   # , subset = .(Taxomony %in% c("Pelicans", "Cormorants"))) +  ## select only a couple spp groups- *change the color options accordingly*
-  scale_x_continuous(name="Population Displacement Vulnerability", limits=c(5,200)) +
-  scale_y_continuous(name="Population Collision Vulnerability", limits=c(5,300)) +
+  scale_x_continuous(name = "Collision Vulnerability") + # , limits=c(5,200)
+  scale_y_continuous(name = "Displacement Vulnerability") + # , limits=c(5,300)
   theme_bw(base_size = 14) + 
-  scale_colour_manual(values=cbbPalette) + # scale_colour_brewer(palette = "Set1") 
+  scale_colour_manual(values=cbbPalette) # + scale_colour_brewer(palette = "Set1") 
 x2
-ggsave("PCVvsPDV.pdf")
+ggsave("CVvDV_point_PVscale.pdf")
 
+## graph CV vs DV by alpha code with PV scaling points
+scores$AlphaCode <- factor(scores$AlphaCode, levels = scores$AlphaCode[order(scores$Order)]) # will keep original order of species
+# scores_hull = scores %>% 
+#   group_by(Taxonomy) %>% 
+#   mutate(hull = 1:n(), hull = factor(hull, chull(ColBest, DispBest))) %>% 
+#   arrange(hull)
+
+scores_hull <- scores
+find_hull <- function(scores_hull) df[chull(scores_hull$ColBest, scores_hull$DispBest), ]
+hulls <- ddply(scores_hull, "issue", find_hull)
+
+x3 <- ggplot(scores, 
+             aes(ColBest,
+                 DispBest,
+                 size = PopBest,
+                 label = as.character(AlphaCode),
+                 color = Taxonomy)) + 
+  geom_text() + # (aes(colour = factor(Taxomony)), size=3,face="bold") +
+  # , subset = .(Taxomony %in% c("Pelicans", "Cormorants"))) +  ## select only a couple spp groups- *change the color options accordingly*
+  scale_x_continuous(name = "Collision Vulnerability") + # , limits=c(5,200)
+  scale_y_continuous(name = "Displacement Vulnerability") + # , limits=c(5,300)
+  theme_bw(base_size = 14) + 
+  scale_colour_manual(values=cbbPalette) # + scale_colour_brewer(palette = "Set1") 
+x3
+ggsave("CVvDV_spp_PVscale.pdf")
+
+
+loc = na.omit(loc) %>%
+  group_by(country) %>%
+  mutate(hull = 1:n(), hull = factor(hull, chull(lat, lon))) %>%
+  arrange(hull)
+
+ggplot(loc, aes(lon, lat, color = country, fill = country)) +
+  geom_polygon(data = filter(loc, !is.na(hull)), alpha = 0.5) +
+  geom_point() +
+  guides(color = FALSE, fill = FALSE) +
+  theme_bw() +
+  theme(axis.text = element_blank())
 #   guides(color = guide_legend(nrow = 2,title = NULL, label.position = "bottom")) +
 #   theme(legend.position = 'bottom')
 
-# plot using points instead of alpha codes
-# x1 <- ggplot(PVscores, 
-#              aes(PDVbest, 
-#                  PCVbest)) + 
-#   geom_point(aes(color=Taxomony), 
-#             size=3, 
-#             face="bold") +  
-#   scale_x_continuous(name="Population Displacement Vulnerability", limits=c(5,200)) +
-#   scale_y_continuous(name="Population Collision Vulnerability", limits=c(5,300)) +
-#   theme_bw(base_size = 14) + 
-#   scale_colour_manual(values=cbbPalette) + # scale_colour_brewer(palette = "Set1") +
-#   guides(color = guide_legend(nrow = 1,title = NULL)) +
-#   theme(legend.position = 'bottom')
-
-#   scale_x_log10(limits = c(5, 1600)) +
-#   scale_y_log10(limits = c(9, 1400), breaks = c(10, 100, 1000)) +
-#   ylab("Population Displacement Vulnerability") + 
-#   xlab("Population Collision Vulnerability") +
-
-#   theme(legend.text = element_text(size=14), 
-#         axis.title.y = element_text(size=rel(1.5)), 
-#         axis.title.x=element_text(size=rel(1.5)))
-
-# x4 <- x3 + theme(legend.position=c("bottom")) # put the legend in the graph, in the top right corn=er
-
-
-## for PCV and PDV scores
-
-# y <- ggplot(PVscores, aes(ColBest, DispBest, label=as.character(AlphaCode))) + geom_text(aes(color=Groups, size=PopBest), face="bold") + theme_bw()
-# y <- y + ylab("Displacement Vulnerability") + xlab("Collision Vulnerability") #  + ylim(0,10) + xlim(0,10)
-# y + theme(legend.text=element_text(size=14), axis.title.y=element_text(size=rel(1.5)), axis.title.x=element_text(size=rel(1.5)))
-
-# y <- ggplot(PVscores, aes(ColBest, DispBest, label=as.character(AlphaCode))) + geom_text(aes(color=Groups), size=4, face="bold") + theme_bw()
-# y <- y + ylab("Displacement Vulnerability") + xlab("Collision Vulnerability") #  + ylim(0,10) + xlim(0,10)
-# y + theme(legend.text=element_text(size=14), axis.title.y=element_text(size=rel(1.5)), axis.title.x=element_text(size=rel(1.5)))
-
-## pie chart of collision sensitivities
-## maintain order
-# scores$AlphaCode <- factor(scores$AlphaCode, levels = scores$AlphaCode[order(scores$Order)]) # will keep original order of species
-
-# CS <- ggplot(scores, aes(x=AlphaCode, fill = ColBest, color="red"))
-# CS <- CS + geom_bar(width=1)+coord_polar()
