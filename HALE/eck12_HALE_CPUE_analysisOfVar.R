455595b29601821638c3ea47485b433d1871b228
Emma <ekelsey@usgs.gov>
2016-11-21 17:09:38 -0800

    working with mlogit

--- HALE/eck12_HALE_CPUE_analysisOfVar.R
+++ HALE/eck12_HALE_CPUE_analysisOfVar.R
@@ -1,24 +1,46 @@
 ## this script statistically analyzes the effect of year, trap location, trap type, and bait type on predator event
 
-library("stats", lib.loc="C:/Program Files/R/R-3.2.3/library")
-library("data.table", lib.loc="~/R/win-library/3.2")
-library("dplyr", lib.loc="~/R/win-library/3.2")
+library(stats)
+library(data.table)
+library(dplyr)
 library(ggplot2)
-library("ez", lib.loc="~/R/win-library/3.2")
+library(ez)
+library(mlogit)
 
 read.csv('~/WERC-SC/HALE/TraplinePredEventPUE_11.csv',
           stringsAsFactors = FALSE) -> predEventPUE
-predEventPUE$Year <- as.character(predEventPUE$Year)
+predEvents <- mlogit.data(predEventPUE, choice = "predEvent", shape = "wide")
 
 ## normal distribution? frequency histogram should be ~symetical, SD of most variable sample shoudl be <10x the SD of the least variable sample
 hist(predEventPUE$CPUE)
 sd(predEventPUE$CPUE)
 
-poi <- glm(NEvents ~ offset(log(NTraps)) + Trapline + Year + Season, data = predEventPUE, family=poisson)
+## Poisson log-linear model
+predEvents %>% 
+  mutate(chid = as.factor(chid),
+         Year = as.factor(Year)) -> predEvents
 
-Qpoi <- glm(NEvents ~ offset(log(NTraps)) + Trapline + Year + Season, data = predEventPUE, family=quasipoisson)
+glm.test <- system.time(                                     # how much time does it take to run the model
+  cpue1 <- glm(predEvent ~ chid + Year + Season + Trapline,
+               family = poisson,
+               data = predEvents)
+)
+tail(coefficients(summary(cpue1)), 5) # no random effects in this model
 
-negB <- glm.nb(NEvents ~ offset(log(NTraps)) + Trapline + Year + Season, data = predEventPUE)
+## mlogit model
+cpue2 <- mlogit(predEvent ~ Year + Season + Trapline, data = predEvents,
+                rpar = c(Year = "n",
+                         Trapline = "n"),
+                panel = TRUE)
+summary(cpue2)
+
+
+
+# poi <- glm(NEvents ~ offset(log(NTraps)) + Trapline + Year + Season, data = predEventPUE, family=poisson)
+# 
+# Qpoi <- glm(NEvents ~ offset(log(NTraps)) + Trapline + Year + Season, data = predEventPUE, family=quasipoisson)
+# 
+# negB <- glm.nb(NEvents ~ offset(log(NTraps)) + Trapline + Year + Season, data = predEventPUE)
 
 # ## anova using the car library
 # ratCaught <- filter(predEventPUE, predEvent == "ratCaught")
@@ -29,8 +51,6 @@ negB <- glm.nb(NEvents ~ offset(log(NTraps)) + Trapline + Year + Season, data = 
 #                  between = c('Year', 'Season'), 
 #                  observed = ('Year'), 
 #                  type = 2)
-# 
-# friedman.test(CPUE ~ year | Trapline, data = predEventPUE, subset = predEvent)
 
 ## anova using normal stats package
 # ratCaught <- filter(predEvent == "ratCaught")
