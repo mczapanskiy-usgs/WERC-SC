67206147fe4c1e4f8317df82c346bfcf93255dfb
Emma <ekelsey@usgs.gov>
2017-06-30 15:52:55 -0700

    output of fitted results

--- HALE/eck17_HALE_CPUE_SeasonYear_mlogit_analysis.R
+++ HALE/eck17_HALE_CPUE_SeasonYear_mlogit_analysis.R
@@ -32,21 +32,22 @@ dev.off()
 
 #### SEASON
 # graph fitted frequencies
-fit_events_season <- ggplot(fitted_events_lg, aes(x=Season, y=value)) +
+ggplot(fitted_events_lg, aes(x=Season, y=value)) +
   geom_boxplot() +
   facet_wrap(~ variable) + #, scales = 'free') +
   scale_x_discrete(limits = c('Pre-laying', 'Incubation', 'Nestling', 'offSeason')) +
   theme_bw() +
-  labs(x = 'Season', y = 'Probability of Events per Unit Effort')
-fit_events_season
+  labs(x = 'Season', y = 'Probability of Events Occurance')
+ggsave(width = 8.5, height = 5, dpi=300, filename = "~/WERC-SC/HALE/outputs/fitSeasonEvents.pdf")
 
-fit_preds_season <- ggplot(fitted_preds_lg, aes(x=Season, y=value)) +
+ggplot(fitted_preds_lg, aes(x=Season, y=value)) +
   geom_boxplot() +
   facet_wrap(~ variable) + #, scales = 'free') +
   scale_x_discrete(limits = c('Pre-laying', 'Incubation', 'Nestling', 'offSeason')) +
   theme_bw() +
-  labs(x = 'Season', y = 'Probability of Events per Unit Effort')
-fit_preds_season
+  labs(x = 'Season', y = 'Probability of Predator Type Caught')
+ggsave(width = 8.5, height = 5, dpi=300, filename = "~/WERC-SC/HALE/outputs/fitSeasonPreds.pdf")
+
 ## tables of mean and SD fitted frequencies
 # seasonal event probabilities
 seasonEvents <- fitted_events %>% 
@@ -70,54 +71,34 @@ write.table(seasonPreds, file = '~/WERC-SC/HALE/outputs/seasonPreds.csv',
 #### YEAR
 ## tables and figures of mean and SD fitted frequencies
 # annual event probabilities
-yearEvents <- fitted_events %>% 
+fitYearEvents <- fitted_events %>% 
   gather(eventType, eventProb, noEvent:otherEvent) %>%
   group_by(Year, eventType) %>% 
   summarize(meanProb = mean(eventProb, na.rm = TRUE), sdProb = sd(eventProb, na.rm = TRUE))
-ggplot(yearEvents, aes(x=Year, y=meanProb, color = eventType)) +
+ggplot(fitYearEvents, aes(x=Year, y=meanProb, color = eventType)) +
   geom_line(size = 1) +
-  geom_point(size = 2) +
+  geom_point(size = 2, shape = 18) +
   geom_errorbar(aes(ymin = meanProb-sdProb, ymax=meanProb+sdProb), width = 0.5) +
-  theme_bw()
-# fit_preds_year <- ggplot(yearEvents, aes(x=Year, y=)) +
-# geom_point(aes(color=Season)) +
-#   theme_bw() +
-#   labs(x = 'Year', y = 'Probability of Events per Unit Effort')
-# fit_preds_year
+  theme_bw() +
+  labs(x = 'Year', y = 'Mean Probability of Event Occurance')
+ggsave(width = 8.5, height = 5, dpi=300, filename = "~/WERC-SC/HALE/outputs/fitYearEvents.pdf")
 
 # annual predEvent probabilities
-# yearPreds <- fitted_preds %>% 
-#   group_by(Year) %>% 
-#   summarize(mRat = mean(ratCaught, na.rm = T), sdRat = sd(ratCaught, na.rm = T),
-#             mCat = mean(catCaught, na.rm = T), sdCat = sd(catCaught, na.rm = T),
-#             mMong = mean(mongooseCaught, na.rm = T), sdMong = sd(mongooseCaught, na.rm = T))
-yearPreds <- fitted_preds %>% 
+fitYearPreds <- fitted_preds %>% 
   gather(predType, predProb, ratCaught:mongooseCaught) %>%
   group_by(Year, predType) %>% 
   summarize(meanProb = mean(predProb, na.rm = TRUE), sdProb = sd(predProb, na.rm = TRUE))
-ggplot(yearPreds, aes(x=Year, y=meanProb, color = predType)) +
+ggplot(fitYearPreds, aes(x=Year, y=meanProb, color = predType)) +
   geom_line(size = 1) +
-  geom_point(size = 2) +
+  geom_point(size = 2, shape = 18) +
   geom_errorbar(aes(ymin = meanProb-sdProb, ymax=meanProb+sdProb), width = 0.5) +
-  theme_bw()
-
-## graph fitted frequencies
-# fit_preds_year <- ggplot(fitted_preds_lg, aes(x=Year, y=value)) +
-#   geom_point(aes(color=Season)) +
-#   facet_wrap(~ variable) + #, scales = 'free') +
-#   theme_bw() +
-#   labs(x = 'Year', y = 'Probability of Events per Unit Effort')
-# fit_preds_year
-# 
-# fit_events_year <- ggplot(fitted_events_lg, aes(x=Year, y=value)) + 
-#   geom_point(aes(color=Season)) +
-#   facet_wrap(~ variable) + #, scales = 'free') +
-#   theme_bw() +
-#   labs(x = 'Year', y = 'Probability of Events per Unit Effort')
-# fit_events_year
+  theme_bw() + 
+  labs(x = 'Year', y = 'Mean Probability of Predator Type Caught')
+ggsave(width = 8.5, height = 5, dpi=300, filename = "~/WERC-SC/HALE/outputs/fitYearPreds.pdf")
 
 # save tables
 write.table(yearEvents, file = '~/WERC-SC/HALE/outputs/yearEvents.csv',
             row.names = FALSE)
 write.table(yearPreds, file = '~/WERC-SC/HALE/outputs/yearPreds.csv',
             row.names = FALSE)
+# save graphs
