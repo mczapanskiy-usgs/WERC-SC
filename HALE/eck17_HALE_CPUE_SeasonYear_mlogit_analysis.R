8c214a0c16b979cabe92f40f212c2d7e76b2c892
Emma <ekelsey@usgs.gov>
2017-06-30 07:48:05 -0700

    analyzing results of eventType and predEvent data
    now that fitted anlysis is done, can analyze all the results

--- HALE/eck17_HALE_CPUE_SeasonYear_mlogit_analysis.R
+++ HALE/eck17_HALE_CPUE_SeasonYear_mlogit_analysis.R
@@ -17,34 +17,63 @@ setwd("~/WERC-SC/HALE")
 #### READ IN FITTED RESULTS FROM BEST FIT MODELS
 # event type (predator, other, no event) data - model w/ traplineYear = random effect, season + year = indiv. specif. was best fit
 read.csv('~/WERC-SC/HALE/fitted_cpue_event_sub.csv',
-         stringsAsFactors = FALSE) <- fitted_events
+         stringsAsFactors = FALSE) -> fitted_events
 # pred event data (rat, cat, or mongoose) data model 23 w/ Trapline = random effect, season + year = indiv. specif. was best fit
 read.csv('~/WERC-SC/HALE/fitted_cpue_model23.csv',
          stringsAsFactors = FALSE) -> fitted_preds
 
 # create long versions of fitted results from mlogit model (variable = pred type, value = fitted CPUE probability)
 fitted_events_lg <- melt(fitted_events, id.vars = c("Trapline", "Year", "Season"),
-                        measure.vars = c("catCaught", "mongooseCaught", "ratCaught"))
+                        measure.vars = c("noEvent", "predatorEvent", "otherEvent"))
 fitted_preds_lg <- melt(fitted_preds, id.vars = c("Trapline", "Year", "Season"),
                        measure.vars = c("catCaught", "mongooseCaught", "ratCaught"))
 dev.off()
 
 #### SEASON
-### use fitted results of mlogit model23 to graph fitted frequencies
+# graph fitted frequencies
+fit_events_season <- ggplot(fitted_events_lg, aes(x=Season, y=value)) +
+  geom_boxplot() +
+  facet_wrap(~ variable) + #, scales = 'free') +
+  scale_x_discrete(limits = c('Pre-laying', 'Incubation', 'Nestling', 'offSeason')) +
+  theme_bw() +
+  labs(x = 'Season', y = 'Probability of Events per Unit Effort')
+fit_events_season
+
 fit_preds_season <- ggplot(fitted_preds_lg, aes(x=Season, y=value)) +
   geom_boxplot() +
-  facet_wrap(~ variable, scales = 'free') +
+  facet_wrap(~ variable) + #, scales = 'free') +
   scale_x_discrete(limits = c('Pre-laying', 'Incubation', 'Nestling', 'offSeason')) +
   theme_bw() +
   labs(x = 'Season', y = 'Probability of Events per Unit Effort')
 fit_preds_season
 
+seasonEvents <- fitted_events %>% 
+  group_by(Season) %>% 
+  summarize(mNone = mean(noEvent, na.rm = T), sdNone = sd(noEvent, na.rm = T),
+            mPred = mean(predatorEvent, na.rm = T), sdPred = sd(predatorEvent, na.rm = T),
+            mOther = mean(otherEvent, na.rm = T), sdOther = sd(otherEvent, na.rm = T))
+
+seasonPreds <- fitted_preds %>% 
+  group_by(Season) %>% 
+  summarize(mRat = mean(ratCaught, na.rm = T), sdRat = sd(ratCaught, na.rm = T),
+            mCat = mean(catCaught, na.rm = T), sdCat = sd(catCaught, na.rm = T),
+            mMong = mean(mongooseCaught, na.rm = T), sdMong = sd(mongooseCaught, na.rm = T))
+  
+
 #### YEAR
-### use fitted results of mlogit model23 to graph fitted frequencies
+## graph fitted frequencies
 fit_preds_year <- ggplot(fitted_preds_lg, aes(x=Year, y=value)) +
   geom_point(aes(color=Season)) +
-  facet_wrap(~ variable, scales = 'free') +
+  facet_wrap(~ variable) + #, scales = 'free') +
   theme_bw() +
   labs(x = 'Year', y = 'Probability of Events per Unit Effort')
 fit_preds_year
+
+fit_events_year <- ggplot(fitted_events_lg, aes(x=Year, y=value)) + 
+  geom_point(aes(color=Season)) +
+  facet_wrap(~ variable) + #, scales = 'free') +
+  theme_bw() +
+  labs(x = 'Year', y = 'Probability of Events per Unit Effort')
+fit_events_year
+
 
