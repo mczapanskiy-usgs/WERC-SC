ae7339889f843b5401b7e76e4953d36c137b4a18
Emma <ekelsey@usgs.gov>
2017-10-16 17:13:59 -0700

    MAMU juvenile ratio analysis and HALE grid analysis

--- HALE/eck24_HALE_indvTrapAnalysis.R
+++ HALE/eck24_HALE_indvTrapAnalysis.R
@@ -1,6 +1,8 @@
 ## This script is to analyze the success of individual traps
 ## to help assess traps are obviously not catching and can be removed.
 
+setwd("~/WERC-SC/HALE")
+
 # load packages
 library(stats)
 library(data.table)
@@ -16,27 +18,26 @@ library(mosaic)
 read.csv('~/WERC-SC/HALE/TrapsGrid20170905.csv',
          stringsAsFactors = FALSE) -> spatialCatch # catch data processed by Ben (elev, slope, prox to roads/trails/fences/structures, veg, etc.) & Jon (grid cells)
 
+# ___________________________________________________________________________
+## GRID
+# PRED EVENT
 # summarize data by trap events in each spatial grid cell (grid500)
 gridEvents <- spatialCatch %>%
   group_by(Grid500, eventType) %>% 
   dplyr::summarise(NEvents = n()) %>% 
   na.omit()
-
 # select just predator events (e.g.- only trap events that caught a predator)
 gridPreds <- gridEvents %>%
   filter(eventType == "predatorEvent") %>% 
   as.data.frame() %>% 
   mutate(quart = ntile(NEvents, 4)) # quart indicates if data is in the bottom (1), top (4), etc. quartile of data
-
 # save data of frequency of predator events within each grid cell and quartiles
 write.csv(gridPreds, file = '~/WERC-SC/HALE/gridPredFrequenciess20170922.csv',
           row.names = FALSE) 
-
 # break predator event data into quartiles and select only the bottom quartile 
 # (e.g.- grid cells with the lowest 25% of predator catches)
 leastGridPreds <- gridPreds %>%
   filter(quart == 1)
-
 # ggplot(leastGridPreds, aes(Grid500, NEvents)) +
 #   geom_bar(stat = "identity") +
 #   theme_bw() +
@@ -47,25 +48,75 @@ leastGridPreds <- gridPreds %>%
 #   theme_bw() +
 #   theme(axis.text.x=element_text(angle = 90))
 
+# RAT, CAT, MONGOOSE
+gridPredEvent <- spatialCatch %>%
+  group_by(Grid500, predEvent) %>% 
+  dplyr::summarise(NEvents = n()) %>% 
+  na.omit()
+# select just predator events (e.g.- only trap events that caught a rat, cat or mongoose)
+gridRat <- gridPredEvent %>%
+  filter(predEvent == "ratCaught") %>% 
+  as.data.frame() %>% 
+  mutate(quart = ntile(NEvents, 4)) # quart indicates if data is in the bottom (1), top (4), etc. quartile of data
+gridCat <- gridPredEvent %>%
+  filter(predEvent == "catCaught") %>% 
+  as.data.frame() %>% 
+  mutate(quart = ntile(NEvents, 4)) 
+gridMongoose <- gridPredEvent %>%
+  filter(predEvent == "mongooseCaught") %>% 
+  as.data.frame() %>% 
+  mutate(quart = ntile(NEvents, 4))
+# # save data of frequency of predator events within each grid cell and quartiles
+# write.csv(gridPreds, file = '~/WERC-SC/HALE/gridPredFrequenciess20170922.csv',
+#           row.names = FALSE) 
 
+
+
+
+# ___________________________________________________________________________
+## TRAP
 # summary data by trap events in each trap number (although these locations change between time periods)
 trapEvents <- spatialCatch %>%
   group_by(trap, predEvent) %>% 
   dplyr::summarise(NEvents = n()) %>% 
   na.omit()
-
 # select just predator events (e.g.- only trap events that caught a predator)
 # break trap data (of pred events) into quartiles 
 trapPreds <- trapEvents %>%
   filter(predEvent %in% c("ratCaught", "catCaught", "mongooseCaught")) %>% 
   as.data.frame() %>% 
   mutate(quart = ntile(NEvents, 4)) # quart indicates if data is in the bottom (1), top (4), etc. quartile of data
-
 # save data of frequency of predator events within each grid cell and quartiles
 write.csv(trapPreds, file = '~/WERC-SC/HALE/trapPredFrequenciess20170922.csv',
           row.names = FALSE) 
-
 # select only the bottom quartile of predator events at each trap
 # (e.g.- trap numbers with the lowest 25% of predator catches)
 leastTrapPreds <- trapPreds %>%
   filter(quart == 1)
+
+
+trapPredEvents <- spatialCatch %>%
+  group_by(trap, predEvent) %>% 
+  dplyr::summarise(NEvents = n()) %>% 
+  na.omit()
+# select just predator events (e.g.- only trap events that caught a predator)
+# break trap data (of pred events) into quartiles 
+trapRat <- trapEvents %>%
+  filter(predEvent == "ratCaught") %>% 
+  as.data.frame() %>% 
+  mutate(quart = ntile(NEvents, 4)) # quart indicates if data is in the bottom (1), top (4), etc. quartile of data
+trapCat <- trapEvents %>%
+  filter(predEvent == "catCaught") %>% 
+  as.data.frame() %>% 
+  mutate(quart = ntile(NEvents, 4)) # quart indicates if data is in the bottom (1), top (4), etc. quartile of data
+trapMongoose <- trapEvents %>%
+  filter(predEvent == "mongooseCaught") %>% 
+  as.data.frame() %>% 
+  mutate(quart = ntile(NEvents, 4)) # quart indicates if data is in the bottom (1), top (4), etc. quartile of data
+# # save data of frequency of predator events within each grid cell and quartiles
+# write.csv(trapPreds, file = '~/WERC-SC/HALE/trapPredFrequenciess20170922.csv',
+#           row.names = FALSE) 
+# # select only the bottom quartile of predator events at each trap
+# # (e.g.- trap numbers with the lowest 25% of predator catches)
+# leastTrapPreds <- trapPreds %>%
+#   filter(quart == 1)
--- MAMU/eck1_MAMU_juvRatio.R
+++ MAMU/eck1_MAMU_juvRatio.R
@@ -1,31 +1,52 @@
 ### JUVENILE RATIO
 # this script calculates the ratio of HY to AHY MAMU on surveys
 
-## set wd
+## SET WORKING DIRECTORY
 setwd("~/WERC-SC/MAMU")
 
-## load libraries
+## LOAD LIBRARIES
 library(data.table)
 library(dplyr)
 library(lubridate)
 library(mosaic)
+library(magrittr)
 
-## read in file
+## READ IN OBSERVATIONS FILE
 read.csv('~/WERC-SC/MAMU/MAMU_Obs_2017_Distance.csv',
          stringsAsFactors = FALSE) -> obs 
-  
 
-
-obs_juv <- obs %>% 
+## SELECT ONLY MAMU DETECTIONS WITHIN JUVENILE TIMEFRAME (JULY 10 - AUGUST 23)
+## AND CALCUALTE ADJUSTED HY AND AHY DETECTIONS 
+obs_mamu <- obs %>% 
+  select(-OBJECTID, -Field1) %>% 
+  distinct() %>% 
   filter(SPP == "MAMU") %>% 
   mutate(date = as_date(date, format = "%m/%d/%Y"), 
          jdate = yday(date)) %>% # add column with Julian Date
   filter(jdate > 191, jdate < 235) %>% # select date between JD 192 - 234 (time frame when juveniles are detected)
+  mutate(HYadj = HY/(-1.5433 + 0.0098 * jdate), # apply regression to HY birds to account for those that haven't fledged by survey date
+         AHYadj = AHY/(1 - (18.7145545 - 0.18445455 * jdate  +  0.00045455 * (jdate^2)))) # apply regression for AHY birds that are still incubting by survey date
 
-         
-juvRatio <- obs_juv %>% 
-  mutate(HYreg = (-1.5433 + 0.0098 * jdate), 
-         HYadj = (HY/HYreg), 
-         AHYreg = 1 - (18.7145545 - 0.18445455 * jdate  +  0.00045455 * (jdate^2)),
-         AHYadj = (AHY/AHYreg),
-         juvRat = sapply()
+## CALUCLATE JUVENILE RATIO FROM ADJUSTED HY AND AHY DETECTION VALUES
+juvRatio <- obs_mamu %>% 
+  group_by(YEAR_) %>% 
+  summarise(n = n_distinct(YEAR_),
+            HYsum = sum(HY, na.rm = TRUE),                  # annual total number of HY
+            HYvar = var(HY, na.rm = TRUE),                  # annual variance in HY
+            HYave = mean(HY, na.rm = TRUE),                 # annual mean  number of HY
+            AHYsum = sum(AHY, na.rm = TRUE),                # annual total number of AHY
+            AHYvar = var(AHY, na.rm = TRUE),                # annual variance in AHY
+            AHYave = mean(AHY, na.rm = TRUE),               # annual mean number of AHY
+            cov = cov(HY, AHY),                             # annual covariance of adjusted 
+            HYsumAdj = sum(HYadj, na.rm = TRUE),            # annual total number of HY per year, adjusted
+            HYvarAdj = var(HYadj, na.rm = TRUE),            # annual variance in adjusted number of HY 
+            HYaveAdj = mean(HYadj, na.rm = TRUE),           # annual mean adjusted number of HY 
+            AHYsumAdj = sum(AHYadj, na.rm = TRUE),          # annual total number of AHY, adjusted
+            AHYvarAdj = var(AHYadj, na.rm = TRUE),          # annual variance in adjusted number of AHY
+            AHYaveAdj = mean(AHYadj, na.rm = TRUE),         # annual mean adjusted number of AHY
+            covAdj = cov(HYadj, AHYadj)) %>%                # annual covariance of adjusted HY and AHY
+  mutate(juvRat = HYsum/AHYsum, # juvenile ratio per survey, adjusted 
+         juvRatAdj = HYsumAdj/AHYsumAdj, # juvenile ratio per survey
+         juvRatVar = (1/n)*(((HYvar/AHYave^2)+(?^2*AHYvar)/AHYave^4)-(((2*HYave)*cov)/AHYave^3))
+         )
+  
