6f1749371478241276592feebfde72c2380560de
Emma <ekelsey@usgs.gov>
2017-10-27 17:15:51 -0700

    edited juv ratio code to share with Julie

--- MAMU/eck1_MAMU_juvRatio.R
+++ MAMU/eck1_MAMU_juvRatio.R
@@ -10,17 +10,17 @@ library(dplyr)
 library(lubridate)
 library(mosaic)
 
-## READ IN OBSERVATIONS FILES
-# all previous years (datafile that Jon got from Bill)
-read.csv('~/WERC-SC/MAMU/MAMU_All_locs_1_03.20.2017_DRAFT.csv',
+### READ IN OBSERVATIONS FILES
+## all previous years (datafile that Jon got from Bill)
+read.csv('~/WERC-SC/MAMU/JuvenileRatioData2013.csv', # MAMU_All_locs_1_03.20.2017_DRAFT.csv',
          stringsAsFactors = FALSE) -> obs_all
-# this year
-read.csv('~/WERC-SC/MAMU/MAMU_Obs_2017_Distance.csv',
+## this year
+read.csv('~/WERC-SC/MAMU/JuvenileRatioData2017.csv', # MAMU_Obs_2017_Distance.csv',
          stringsAsFactors = FALSE) -> obs_2017
 
-## SELECT ONLY MAMU DETECTIONS WITHIN JUVENILE TIMEFRAME (JULY 10 - AUGUST 23)
-## AND CALCUALTE ADJUSTED HY AND AHY DETECTIONS 
-# previous years
+### SELECT ONLY MAMU DETECTIONS WITHIN JUVENILE TIMEFRAME (JULY 10 - AUGUST 23)
+### AND CALCUALTE ADJUSTED HY AND AHY DETECTIONS 
+## previous years
 obs_mamu_all <- obs_all %>% 
   mutate(date = as_date(Date, format = "%m/%d/%Y"), 
          jdate = yday(date)) %>% # add column with Julian Date
@@ -28,7 +28,7 @@ obs_mamu_all <- obs_all %>%
   mutate(HYadj = HY/(-1.5433 + 0.0098 * jdate), # apply regression to HY birds to account for those that haven't fledged by survey date
          AHYadj = AHY/(1 - (18.7145545 - 0.18445455 * jdate  +  0.00045455 * (jdate^2))), # apply regression for AHY birds that are still incubting by survey date
          Surv_No = as.numeric(as.factor(date)))
-# this year
+## this year
 obs_mamu_2017 <- obs_2017 %>%
   select(-OBJECTID, -Field1) %>%
   distinct() %>%
@@ -39,13 +39,8 @@ obs_mamu_2017 <- obs_2017 %>%
   mutate(HYadj = HY/(-1.5433 + 0.0098 * jdate), # apply regression to HY birds to account for those that haven't fledged by survey date
          AHYadj = AHY/(1 - (18.7145545 - 0.18445455 * jdate  +  0.00045455 * (jdate^2)))) # apply regression for AHY birds that are still incubting by survey date
 
-test <- obs_mamu %>%  
-  group_by(Year) %>% 
-  distinct()
-
-
-## CALUCLATE JUVENILE RATIO FROM ADJUSTED HY AND AHY DETECTION VALUES
-# previous years
+### CALUCLATE JUVENILE RATIO FROM ADJUSTED HY AND AHY DETECTION VALUES
+## previous years
 juvRatio_all <- obs_mamu_all %>% 
   group_by(Year) %>%
   summarise(nSvy = n_distinct(Surv_No),
@@ -59,13 +54,15 @@ juvRatio_all <- obs_mamu_all %>%
          # cov = cov(HYsum, AHYsum, use = "pairwise.complete.obs"),
          # covAdj = cov(HYsumAdj, AHYsumAdj, use = "pairwise.complete.obs"),
          juvRat = HYsum/AHYsum, # juvenile ratio per survey, adjusted 
-         juvRatSE = (1/nSvy)*(HYvar/AHYvar),
-         juvRatSEadj = (1/nSvy)*(HYvarAdj/AHYvarAdj),
+         # juvRatSE = (1/nSvy)*(HYvar/AHYvar),
+         # juvRatSEadj = (1/nSvy)*(HYvarAdj/AHYvarAdj),
          juvRatAdj = HYsumAdj/AHYsumAdj, # juvenile ratio per survey
-         juvRatVar = (1/nSvy)*(((HYvar/(AHYave^2))+((HYave^2)*AHYvar)/(AHYave^4))-(((2*HYave)*cov)/(AHYave^3))),
-         juvRatVarAdj = (1/nSvy)*(((HYvarAdj/(AHYaveAdj^2))+((HYaveAdj^2)*AHYvarAdj)/(AHYaveAdj^4))-(((2*HYaveAdj)*covAdj)/(AHYaveAdj^3))))
+         juvRatVar = (((HYvar/(AHYave^2))+((HYave^2)*AHYvar)/(AHYave^4))-(((2*HYave)*cov)/(AHYave^3)))/nSvy,
+         juvRatVarAdj = (((HYvarAdj/(AHYaveAdj^2))+((HYaveAdj^2)*AHYvarAdj)/(AHYaveAdj^4))-(((2*HYaveAdj)*covAdj)/(AHYaveAdj^3)))/nSvy,
+         juvRatSE = juvRatVar^(0.5),
+         juvRatSEadj = juvRatVarAdj^(0.5))
 
-# this year
+## this year
 juvRatio_2017 <- obs_mamu_2017 %>%
   group_by(YEAR_) %>%
   summarise(nSvy = n_distinct(Surv_No),
@@ -80,19 +77,21 @@ juvRatio_2017 <- obs_mamu_2017 %>%
   #        covAdj = cov(HYsumAdj, AHYsumAdj, use = "pairwise.complete.obs"),
          juvRat = HYsum/AHYsum, # juvenile ratio per survey, adjusted
          juvRatAdj = HYsumAdj/AHYsumAdj, # juvenile ratio per survey
-         juvRatSE = (1/nSvy)*(HYvar/AHYvar),
-         juvRatSEadj = (1/nSvy)*(HYvarAdj/AHYvarAdj),
-         juvRatVar = (1/nSvy)*(((HYvar/(AHYave^2))+((HYave^2)*AHYvar)/(AHYave^4))-(((2*HYave)*cov)/(AHYave^3))),
-         juvRatVarAdj = (1/nSvy)*(((HYvarAdj/(AHYaveAdj^2))+((HYaveAdj^2)*AHYvarAdj)/(AHYaveAdj^4))-(((2*HYaveAdj)*covAdj)/(AHYaveAdj^3))))
+         # juvRatSE = (1/nSvy)*(HYvar/AHYvar),
+         # juvRatSEadj = (1/nSvy)*(HYvarAdj/AHYvarAdj),
+         juvRatVar = (((HYvar/(AHYave^2))+((HYave^2)*AHYvar)/(AHYave^4))-(((2*HYave)*cov)/(AHYave^3)))/nSvy,
+         juvRatVarAdj = (((HYvarAdj/(AHYaveAdj^2))+((HYaveAdj^2)*AHYvarAdj)/(AHYaveAdj^4))-(((2*HYaveAdj)*covAdj)/(AHYaveAdj^3)))/nSvy,
+         juvRatSE = juvRatVar^(0.5),
+         juvRatSEadj = juvRatVarAdj^(0.5))
 
-# save juvenile ratio results and statistics
+## save juvenile ratio results and statistics
 write.csv(juvRatio_all, file = '~/WERC-SC/MAMU/juvRatio_all.csv',
           row.names = FALSE) 
 write.csv(juvRatio_2017, file = '~/WERC-SC/MAMU/juvRatio_2017.csv',
           row.names = FALSE) 
-write.csv(obs_mamu_2017, file = '~/WERC-SC/MAMU/JuvenileRatio_AllData_2017.csv',
-          row.names = FALSE) 
+# write.csv(obs_mamu_2017, file = '~/WERC-SC/MAMU/JuvenileRatio_AllData_2017.csv',
+#           row.names = FALSE) 
 
-rbind(juvRatio_all, juvRatio_2017) -> juvRatio
+# rbind(juvRatio_all, juvRatio_2017) -> juvRatio
 
   
