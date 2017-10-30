7ed3cdc3c2daf4243fbce8786f32e4d4b0b27f8a
Emma <ekelsey@usgs.gov>
2017-10-30 15:57:18 -0700

    finished reformatting monitoring database

--- monitoringDB/reformatDB.r
+++ monitoringDB/reformatDB.r
@@ -19,19 +19,40 @@ read.csv('~/WERC-SC/monitoringDB/SeabirdStudyContents_old.csv',
          stringsAsFactors = FALSE) -> studyContents_old
 read.csv('~/WERC-SC/monitoringDB/SeabirdMetadata_old.csv',
          stringsAsFactors = FALSE) -> metadata_old
+read.csv('~/WERC-SC/monitoringDB/sppNames.csv',
+         stringsAsFactors = FALSE) -> sppNames
+
 
 ## reformat study contents data so that theres a separate row for every method/spp combination
-studyContents <- gather(studyContents_old, Method, Species, ColonyCount:OtherDataTypeMethod) %>% 
+studyContents <- gather(studyContents_old, DataCollected, Species, ColonyCount:OtherDataTypeMethod) %>% 
   cSplit('Species', sep=", ", type.convert=FALSE) %>% 
   gather(SppCount, Species, Species_01:Species_24) %>% 
-  select(-SppCount, -CreatedDate)
+  select(-SppCount, -CreatedDate) %>% 
+  filter(!is.na(Species))
+
+studyContents$Species <- gsub(",","",studyContents$Species,
+                              " ","",studyContents$Species)
 
 
-test <- metadata_old %>% 
-  select(Affiliation:Jurisdiction)
+# bind to species name
+studyContents2 <- left_join(studyContents, sppNames, by = c("Species" = "AlphaCode")) %>% 
+  mutate(AlphaCode = Species,
+         Taxa = Family) %>% 
+  select(-Family, -Order, -Species)
 
-metaData <- test %>% 
+studyContents3 <- left_join(studyContents2, metadata_old, by = "SeabirdSurveyID") %>% 
   cSplit('StudyRegion', sep=", ", type.convert=FALSE) %>% 
-  gather(State, StudyRegion, StudyRegion_1:StudyRegion_9) %>% 
-  group_by(Affiliation)
+  gather(regionCount, StudyRegion, StudyRegion_1:StudyRegion_9) %>%
+  select(-regionCount) %>% 
+  filter(!is.na(StudyRegion)) %>% 
+  select(DataCollectedID, SeabirdSurveyID, AlphaCode, Taxa, SpeciesName, ScientificName, StateName, StudyRegion, MonitorSite, 
+         Jurisdiction, DataCollected, StartYear:DataCollectionFrequency, Notes) 
+  
+
+studyContents3$StudyRegion <- gsub(",","",studyContents3$StudyRegion,
+                                   " ","",studyContents3$StudyRegion)
+
+## save updated database
+write.csv(studyContents3, file = '~/WERC-SC/monitoringDB/SeabirdStudyContents_new.csv',
+          row.names = FALSE)
 
