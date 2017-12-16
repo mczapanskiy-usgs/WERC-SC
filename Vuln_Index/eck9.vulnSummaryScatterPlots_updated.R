f9d3979b80dd3b3fc91a5e94dc181168226ecb5e
Emma <ekelsey@usgs.gov>
2017-12-15 17:31:14 -0800

    updating spatial analysis of vuln index

--- Vuln_Index/eck9.vulnSummaryScatterPlots_updated.R
+++ Vuln_Index/eck9.vulnSummaryScatterPlots_updated.R
@@ -7,26 +7,18 @@ library(ggplot2)
 library(jpeg)
 library(plyr)
 library(dplyr)
+library(extrafont)
 
 #### load data
-### PCV, PDV scores
-PVscores <- read.csv("PCV&PDVscores_20171207.csv") ## matrix of cumulative PV, CPV, and DPV before 1-10 ranking
-PVscores <-   PVscores[complete.cases(PVscores), ] %>% # remove blank observations at bottom of matrix
-  mutate(AlphaCode = factor(AlphaCode, levels = AlphaCode[order(TaxNumCl)])) 
-# PVscores_old <- read.csv("PCV&PDVscores_old.csv")
-#   PVscores_old <-   PVscores_old[complete.cases(PVscores_old), ]
-### PV, CV, DV scores
-scores <- read.csv("PV.CV.DVscores_20171207.csv") %>% ## matrix of final PV, CV, and DV
+scores <- read.csv("vulnScores_20171214.csv") %>% ## matrix of final PV, CV, and DV
   mutate(AlphaCode = factor(AlphaCode, levels = AlphaCode[order(TaxNumCl)])) # will keep original order of species
-
 
 #### establish color palettes
 cbbPalette <- c("#666666", "#66FF00", "#FF0033", "#56B4E9", "#0072B2", "#6600CC", "#E69F00", "#009E73", "#D55E00", "#FF33CC", "#000000") # http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/
-# cbbPalette <- c("#FF33CC", "#6600CC", "#FF0033", "#D55E00", "#E69F00", "#66FF00", "#009E73", "#0072B2", "#56B4E9", "#666666", "#000000") # http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/
 
 #### graph PCV vs PDV with species names as points
-PVscores$AlphaCode <- factor(PVscores$AlphaCode, levels = PVscores$AlphaCode[order(PVscores$TaxNumCl)]) # will keep original order of species
-x1 <- ggplot(PVscores, aes(PDVbest, PCVbest, 
+# scores$AlphaCode <- factor(scores$AlphaCode, levels = scores$AlphaCode[order(scores$TaxNumCl)]) # will keep original order of species
+x1 <- ggplot(scores, aes(PDVbest, PCVbest, 
                  label=as.character(AlphaCode))) + # SHOULD MAKE LETTERS BOLD
   geom_text(aes(color=Taxonomy), size=3, face="bold") +
             # , subset = .(Taxomony %in% c("Pelicans", "Cormorants"))) +  ## select only a couple spp groups- *change the color options accordingly*
@@ -37,28 +29,50 @@ x1 <- ggplot(PVscores, aes(PDVbest, PCVbest,
 x1
 ggsave("PCVvsPDV_20170523.pdf", width = 12, height = 8)
 
-
-
 #### graph PVvCV and PVvDV
-# scores$AlphaCode <- factor(scores$AlphaCode, levels = scores$AlphaCode[order(scores$TaxNumCl)]) # will keep original order of species
-CV <- ggplot(scores,aes(PVbest, CVbest, label=as.character(AlphaCode))) +
+CV <- ggplot(scores,aes(CVbest, PVbest, label=as.character(AlphaCode))) +
   geom_text(aes(color=Taxonomy),
-            size=3,
-            face="bold") +
-  # , subset = .(Taxomony %in% c("Pelicans", "Cormorants"))) +  ## select only a couple spp groups- *change the color options accordingly*
-  scale_x_continuous(name="Population Displacement Vulnerability", limits=c(5,200)) +
-  scale_y_continuous(name="Population Collision Vulnerability", limits=c(5,300)) +
+            size=5, face="bold") +
+  scale_x_continuous(name="Collision Vulnerability", limits=c(0,15)) + # min = 3, max = 15
+  scale_y_continuous(name="Population Vulnerability", limits=c(0,30)) + # min = 4, max = 30
   theme_bw(base_size = 14) +
   scale_colour_manual(values=cbbPalette) # + scale_colour_brewer(palette = "Set1")
 CV
-ggsave("PCVvsPDV_old.pdf", width = 12, height = 8)
+ggsave(width = 12, height = 8, filename = "~/WERC-SC/Vuln_Index/outputs/CVvsPV.pdf")
 
+DV <- ggplot(scores,aes(DVbest, PVbest, label=as.character(AlphaCode))) +
+  geom_text(aes(color=Taxonomy),
+            size=5, face="bold") +
+  scale_x_continuous(name="Displacement Vulnerability", limits=c(0,10)) + # min = 3, max = 15
+  scale_y_continuous(name="Population Vulnerability", limits=c(0,30)) + # min = 4, max = 30
+  theme_bw(base_size = 14) +
+  scale_colour_manual(values=cbbPalette) # + scale_colour_brewer(palette = "Set1")
+DVpercent
+ggsave(width = 12, height = 8, filename = "~/WERC-SC/Vuln_Index/outputs/DVvsPV.pdf")
 
+## percentages
+CVpercent<- ggplot(scores,aes(CVpct, PVpct, label=as.character(AlphaCode))) +
+  geom_text(aes(color=Taxonomy),
+            size=5, face="bold") +
+  scale_x_continuous(name="Collision Vulnerability", limits=c(0,1)) + # min = 3, max = 15
+  scale_y_continuous(name="Population Vulnerability", limits=c(0,1)) + # min = 4, max = 30
+  theme_bw(base_size = 14) +
+  scale_colour_manual(values=cbbPalette) # + scale_colour_brewer(palette = "Set1")
+CVpercent
+ggsave(width = 12, height = 8, filename = "~/WERC-SC/Vuln_Index/outputs/CVvsPV_pct.pdf")
+
+DVpercent <- ggplot(scores,aes(DVpct, PVpct, label=as.character(AlphaCode))) +
+  geom_text(aes(color=Taxonomy),
+            size=5, face="bold") +
+  scale_x_continuous(name="Displacement Vulnerability", limits=c(0,1)) + # min = 3, max = 15
+  scale_y_continuous(name="Population Vulnerability", limits=c(0,1)) + # min = 4, max = 30
+  theme_bw(base_size = 14) +
+  scale_colour_manual(values=cbbPalette) # + scale_colour_brewer(palette = "Set1")
+DVpercent
+ggsave(width = 12, height = 8, filename = "~/WERC-SC/Vuln_Index/outputs/DVvsPV_pct.pdf")
 
 
 ## graph CV vs DV colored by species group with convex hull polygons around species groups
-# keep original order of species
-scores$AlphaCode <- factor(scores$AlphaCode, levels = scores$AlphaCode[order(scores$Order)]) 
 # make convex hull
 scores_hull <- scores
 find_hull <- function(scores) scores[chull(scores$ColBest, scores$DispBest), ]
--- Vuln_Index/spatial/eck1_vulnDensity.R
+++ Vuln_Index/spatial/eck1_vulnDensity.R
@@ -5,56 +5,102 @@
 
 setwd("~/WERC-SC/Vuln_Index/spatial")
 
-library("data.table", lib.loc="~/R/win-library/3.2")
-library("dplyr", lib.loc="~/R/win-library/3.2")
+library(data.table)
+library(dplyr)
 library(tidyr)
 
 
-# read in files
-read.csv('~/WERC-SC/Vuln_Index/spatial/SoCAvulnScores_20170524.csv',
+### read in files
+read.csv('~/WERC-SC/Vuln_Index/spatial/SoCAvulnScores_20171214.csv',
          stringsAsFactors = FALSE) -> vulnScores
+read.csv('~/WERC-SC/Vuln_Index/spatial/SoCAvulnPcts_20171215.csv',
+         stringsAsFactors = FALSE) %>% 
+  mutate(PCV = PCV*100,
+         PDV = PDV*100) -> vulnPcts
 read.csv('~/WERC-SC/Vuln_Index/spatial/SoCA5minDensities.csv',
          stringsAsFactors = FALSE) -> densities
 densities <- select(densities, -ALL_BIRDS)
 
-# create species name string
+### create species name string
+## scores
 spp <- as.vector(vulnScores$SurveySpp)
 PCVspp <- paste(spp, "PCV", "density", sep = "_")
-PDVspp <- paste(spp, "PDV", "density", sep = "_")  
+PDVspp <- paste(spp, "PDV", "density", sep = "_") 
+## percents
+spp <- as.vector(vulnPcts$SurveySpp)
+PCVspp <- paste(spp, "PCV", "density", sep = "_")
+PDVspp <- paste(spp, "PDV", "density", sep = "_") 
 
-# vuln score percent rank
+### vuln score percent rank
 vulnRanks <- mutate(vulnScores,
                     PCVrank = percent_rank(PCV), 
                     PDVrank = percent_rank(PDV))
 
-# density percent rank and merge with vulnRanks
+### density percent rank and merge with vulnRanks
+## scores -> percent rank
 densities_long <- gather(densities, species, dens, ASSP:XAMU) %>% # change to long format (tidyr)
   mutate(densityRank = percent_rank(dens)) %>% # add percent rank of all survey densities
   merge(vulnRanks, by.x = "species", by.y = "SurveySpp") %>%  # merge with vulnRanks table
   mutate(rankDensPCV = densityRank*PCVrank,
          rankDensPDV = densityRank*PDVrank)
+## percents
+density_pct_long <- gather(densities, species, dens, ASSP:XAMU) %>% # change to long format (tidyr)
+  mutate(densityRank = percent_rank(dens)) %>% # add percent rank of all survey densities
+  merge(vulnPcts, by.x = "species", by.y = "SurveySpp") %>%  # merge with vulnRanks table
+  mutate(pctDensPCV = densityRank*PCV,
+         pctDensPDV = densityRank*PDV)
 
+### create tables to import into ARC
+## PERCENT RANK
+# PCV
 ranksDensityPCV <- select(densities_long, species, TGRIDALB_I, rankDensPCV) %>% 
   spread(species, rankDensPCV) %>% 
   replace(is.na(.), 0) %>%
   mutate(ALL_PCV_density = rowSums(.[2:66])) %>% 
   setnames(old = c(spp), new = c(PCVspp))
-
+# PDV
 ranksDensityPDV <- select(densities_long, species, TGRIDALB_I, rankDensPDV) %>% 
   spread(species, rankDensPDV) %>% 
   replace(is.na(.), 0) %>%
   mutate(ALL_PDV_density = rowSums(.[2:66])) %>% 
   setnames(old = c(spp), new = c(PDVspp))
 
+## PERCENTAGES BASED ON SCORE RANGES
+# PCV
+pctDensityPCV <- select(density_pct_long, species, TGRIDALB_I, pctDensPCV) %>% 
+  spread(species, pctDensPCV) %>% 
+  replace(is.na(.), 0) %>%
+  mutate(ALL_PCV_density = rowSums(.[2:66])) %>% 
+  setnames(old = c(spp), new = c(PCVspp))
+# PDV
+pctDensityPDV <- select(density_pct_long, species, TGRIDALB_I, pctDensPDV) %>% 
+  spread(species, pctDensPDV) %>% 
+  replace(is.na(.), 0) %>%
+  mutate(ALL_PDV_density = rowSums(.[2:66])) %>% 
+  setnames(old = c(spp), new = c(PDVspp))
+
 ranksDensity <- inner_join(ranksDensityPCV, ranksDensityPDV, by = "TGRIDALB_I")
+pctDensity <- inner_join(pctDensityPCV, pctDensityPDV, by = "TGRIDALB_I")
 
-
-## save ranksDensityPCV and ranksDensityPDV data file to GitHub file
+### save ranksDensityPCV and ranksDensityPDV data file to GitHub file
+## PERCENT RANKS
+# PCV
 write.csv(ranksDensityPCV, file = '~/WERC-SC/Vuln_Index/spatial/ranksDensityPCV_soCal_20170524.csv',
           row.names = FALSE) 
-
+# PDV 
 write.csv(ranksDensityPDV, file = '~/WERC-SC/Vuln_Index/spatial/ranksDensityPDV_soCal_20170524.csv',
           row.names = FALSE) 
-
+# BOTH
 write.csv(ranksDensity, file = '~/WERC-SC/Vuln_Index/spatial/ranksDensity_soCal_20170602.csv',
+          row.names = FALSE) 
+
+## PERCENTAGES BASED ON SCORE RANGES
+# PCV
+write.csv(pctDensityPCV, file = '~/WERC-SC/Vuln_Index/spatial/pctDensityPCV_soCal_20171215.csv',
+          row.names = FALSE) 
+# PDV 
+write.csv(pctDensityPDV, file = '~/WERC-SC/Vuln_Index/spatial/pctDensityPDV_soCal_20171215.csv',
+          row.names = FALSE) 
+# BOTH
+write.csv(pctDensity, file = '~/WERC-SC/Vuln_Index/spatial/pctDensity_soCal_20171215.csv',
           row.names = FALSE) 
